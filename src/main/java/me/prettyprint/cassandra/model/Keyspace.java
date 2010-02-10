8d40cf90cec35d152669ecdd006c034eeef4729f
Ran Tavory <rantav@gmail.com>
2010-02-11 00:32:05 +0200

    Add interfaces, first revision

---
+++ src/main/java/me/prettyprint/cassandra/model/Keyspace.java
@@ -0,0 +1,185 @@
+package me.prettyprint.cassandra.model;
+
+import java.util.List;
+import java.util.Map;
+
+import me.prettyprint.cassandra.service.CassandraClient;
+
+import org.apache.cassandra.service.Column;
+import org.apache.cassandra.service.ColumnParent;
+import org.apache.cassandra.service.ColumnPath;
+import org.apache.cassandra.service.InvalidRequestException;
+import org.apache.cassandra.service.NotFoundException;
+import org.apache.cassandra.service.SlicePredicate;
+import org.apache.cassandra.service.SuperColumn;
+import org.apache.cassandra.service.TimedOutException;
+import org.apache.cassandra.service.UnavailableException;
+import org.apache.thrift.TException;
+
+/**
+ * The keyspace is a high level handle to all read/write operations to cassandra
+ *
+ * @author rantav
+ */
+public interface Keyspace {
+
+  /**
+   * @return The cassandra client object used to obtain this KeySpace.
+   */
+  public CassandraClient getClient();
+
+  /**
+   * Get the Column at the given columnPath.
+   *
+   * If no value is present, NotFoundException is thrown.
+   *
+   * @throws NotFoundException
+   *           if no value exists for the column
+   */
+  public Column getColumn(String key, ColumnPath columnPath) throws InvalidRequestException,
+      NotFoundException, UnavailableException, TException, TimedOutException;
+
+  /**
+   * Get the SuperColumn at the given columnPath.
+   *
+   * If no value is present, NotFoundException is thrown.
+   *
+   * by default will return column with native order and the size of the list is
+   * unlimited (so be careful...)
+   *
+   * @throws NotFoundException
+   *           when a supercolumn is not found
+   */
+  public SuperColumn getSuperColumn(String key, ColumnPath columnPath)
+      throws InvalidRequestException, NotFoundException, UnavailableException, TException,
+      TimedOutException;
+
+  /**
+   * Get the SuperColumn at the given columnPath.
+   *
+   * If no value is present, NotFoundException is thrown.
+   *
+   * by default will return column with native order and the size of the list is
+   * unlimited (so be careful...)
+   *
+   * @param reversed
+   *          the result Column sort
+   * @param size
+   *          the result column size
+   * @throws NotFoundException
+   *           when a supercolumn is not found
+   */
+  public SuperColumn getSuperColumn(String key, ColumnPath columnPath, boolean reversed, int size)
+      throws InvalidRequestException, NotFoundException, UnavailableException, TException,
+      TimedOutException;
+
+  /**
+   * Get the group of columns contained by columnParent.
+   *
+   * Returns Either a ColumnFamily name or a ColumnFamily/SuperColumn specified
+   * by the given predicate. If no matching values are found, an empty list is
+   * returned.
+   */
+  public List<Column> getSlice(String key, ColumnParent columnParent, SlicePredicate predicate)
+      throws InvalidRequestException, NotFoundException, UnavailableException, TException,
+      TimedOutException;
+
+  /**
+   * Get the group of superColumn contained by columnParent.
+   */
+  public List<SuperColumn> getSuperSlice(String key, ColumnParent columnParent,
+      SlicePredicate predicate) throws InvalidRequestException, NotFoundException,
+      UnavailableException, TException, TimedOutException;
+
+  /**
+   * Performs a get for columnPath in parallel on the given list of keys.
+   *
+   * The return value maps keys to the Column found. If no value corresponding
+   * to a key is present, the key will still be in the map, but both the column
+   * and superColumn references of the ColumnOrSuperColumn object it maps to
+   * will be null.
+   */
+  public Map<String, Column> multigetColumn(List<String> keys, ColumnPath columnPath)
+      throws InvalidRequestException, UnavailableException, TException, TimedOutException;
+
+  /**
+   * Performs a get for columnPath in parallel on the given list of keys.
+   *
+   * The return value maps keys to the ColumnOrSuperColumn found. If no value
+   * corresponding to a key is present, the key will still be in the map, but
+   * both the column and superColumn references of the ColumnOrSuperColumn
+   * object it maps to will be null.
+   */
+  public Map<String, SuperColumn> multigetSuperColumn(List<String> keys, ColumnPath columnPath)
+      throws InvalidRequestException, UnavailableException, TException, TimedOutException;
+
+  /**
+   * Perform a get for columnPath in parallel on the given list of keys.
+   *
+   * The return value maps keys to the ColumnOrSuperColumn found. If no value
+   * corresponding to a key is present, the key will still be in the map, but
+   * both the column and superColumn references of the ColumnOrSuperColumn
+   * object it maps to will be null.
+   */
+  public Map<String, SuperColumn> multigetSuperColumn(List<String> keys, ColumnPath columnPath,
+      boolean reversed, int size) throws InvalidRequestException, UnavailableException, TException,
+      TimedOutException;
+
+  /**
+   * Performs a get_slice for columnParent and predicate for the given keys in
+   * parallel.
+   */
+  public Map<String, List<Column>> multigetSlice(List<String> keys, ColumnParent columnParent,
+      SlicePredicate predicate) throws InvalidRequestException, UnavailableException, TException,
+      TimedOutException;
+
+  /**
+   * Performs a get_slice for a superColumn columnParent and predicate for the
+   * given keys in parallel.
+   */
+  public Map<String, List<SuperColumn>> multigetSuperSlice(List<String> keys,
+      ColumnParent columnParent, SlicePredicate predicate) throws InvalidRequestException,
+      UnavailableException, TException, TimedOutException;
+
+  /**
+   * Inserts a column.
+   */
+  public void insert(String key, ColumnPath columnPath, byte[] value)
+      throws InvalidRequestException, UnavailableException, TException, TimedOutException;
+
+  /**
+   * Insert Columns or SuperColumns across different Column Families for the same row key.
+   */
+  public void batchInsert(String key, Map<String, List<Column>> cfmap,
+      Map<String, List<SuperColumn>> superColumnMap) throws InvalidRequestException,
+      UnavailableException, TException, TimedOutException;
+
+  /**
+   * Remove data from the row specified by key at the columnPath.
+   *
+   * Note that all the values in columnPath besides columnPath.column_family are truly optional:
+   * you can remove the entire row by just specifying the ColumnFamily, or you can remove
+   * a SuperColumn or a single Column by specifying those levels too.
+   */
+  public void remove(String key, ColumnPath columnPath) throws InvalidRequestException,
+      UnavailableException, TException, TimedOutException;
+
+  /**
+   * get a description of the specified keyspace
+   */
+  public Map<String, Map<String, String>> describeKeyspace() throws NotFoundException, TException;
+
+  /**
+   * Counts the columns present in columnParent.
+   */
+  public int getCount(String key, ColumnParent columnParent) throws InvalidRequestException,
+      UnavailableException, TException, TimedOutException;
+
+  /**
+   * returns a subset of columns for a range of keys.
+   */
+  public List<String> getRangeSlice(ColumnParent columnParent, SlicePredicate predicate,
+      String start, String finish, int count)
+      throws InvalidRequestException, UnavailableException, TException, TimedOutException;
+
+}
---
+++ src/main/java/me/prettyprint/cassandra/service/CassandraClient.java
@@ -0,0 +1,80 @@
+package me.prettyprint.cassandra.service;
+
+import java.util.List;
+
+import me.prettyprint.cassandra.model.Keyspace;
+
+import org.apache.cassandra.service.ConsistencyLevel;
+import org.apache.cassandra.service.NotFoundException;
+import org.apache.thrift.TException;
+
+
+/**
+ * Client object, a high level handle to the remove cassandra service.
+ * <p>
+ * From a client you can obtain a Keyspace. A keyspace lets you write/read the remote cassandra.
+ *
+ * @author rantav
+ */
+public interface CassandraClient {
+
+  static final int DEFAULT_CONSISTENCY_LEVEL = ConsistencyLevel.DCQUORUM;
+
+//  /**
+//   * @return the underline cassandra thrift object, all remote calls will be sent to this client.
+//   */
+//  Cassandra.Client getCassandra();
+
+  /**
+   * Return given key space, if keySpaceName not exist, will throw an exception.
+   * <p>
+   * Thread safety: not safe ;-)
+   * Really, if you require thread safety do it at the application level, this class does not
+   * provide it.
+   * <p>
+   * Uses the default consistency level, {@link #DEFAULT_CONSISTENCY_LEVEL}
+   */
+  Keyspace getKeySpace(String keySpaceName)
+      throws IllegalArgumentException, NotFoundException, TException;
+
+
+  /**
+   * Gets s keyspace with the specified consistency level.
+   */
+  Keyspace getKeySpace(String keySpaceName, ConsistencyLevel consitencyLevel)
+      throws IllegalArgumentException, NotFoundException, TException;
+
+
+
+  /**
+   * Gets a string property from the server, such as:
+   * "cluster name": cluster name;
+   * "config file" : all config file content, if need you can try to explain it.
+   * "token map" :  get the token map from local gossip protocal.
+   */
+  String getStringProperty(String propertyName) throws TException;
+
+
+  /**
+   * @return all keyspaces name of this client.
+   */
+  List<String> getKeyspaces();
+
+
+  /**
+   * @return target server cluster name
+   */
+  String getClusterName();
+
+
+  /**
+   * @return the tokens map.
+   */
+  String getTokenMap();
+
+
+  /**
+   * @return config file content.
+   */
+  String getConfigFile();
+}
