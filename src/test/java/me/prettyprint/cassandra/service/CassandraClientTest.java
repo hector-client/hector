20a9990866a8f385ca90746b3a9746e880729d52
Ran Tavory <rantav@gmail.com>
2010-02-11 12:04:11 +0200

    Refactor the embedded server stuff out of the test so it can be reused by other tests

--- src/test/java/me/prettyprint/cassandra/service/CassandraClientTest.java
+++ src/test/java/me/prettyprint/cassandra/service/CassandraClientTest.java
@@ -1,21 +1,18 @@
 package me.prettyprint.cassandra.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import me.prettyprint.cassandra.model.Keyspace;
+import me.prettyprint.cassandra.testutils.EmbeddedServerHelper;
 
-import org.apache.cassandra.contrib.utils.service.CassandraServiceDataCleaner;
 import org.apache.cassandra.service.ConsistencyLevel;
-import org.apache.cassandra.service.EmbeddedCassandraService;
 import org.apache.cassandra.service.NotFoundException;
-import org.apache.cassandra.utils.FileUtils;
 import org.apache.thrift.TException;
 import org.apache.thrift.transport.TTransportException;
 import org.junit.AfterClass;
@@ -30,9 +27,7 @@ import org.junit.Test;
  */
 public class CassandraClientTest {
 
-  private static EmbeddedCassandraService cassandra;
-
-  private static final String TMP = "tmp";
+  private static EmbeddedServerHelper embedded;
 
   private CassandraClient client;
 
@@ -45,27 +40,13 @@ public class CassandraClientTest {
    */
   @BeforeClass
   public static void setup() throws TTransportException, IOException, InterruptedException {
-    // delete tmp dir first
-    rmdir(TMP);
-    // make a tmp dir and copy storag-conf.xml and log4j.properties to it
-    copy("/storage-conf.xml", TMP);
-    copy("/log4j.properties", TMP);
-    System.setProperty("storage-config", TMP);
-
-    CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
-    cleaner.prepare();
-    cassandra = new EmbeddedCassandraService();
-    cassandra.init();
-    Thread t = new Thread(cassandra);
-    t.setDaemon(true);
-    t.start();
+    embedded = new EmbeddedServerHelper();
+    embedded.setup();
   }
 
   @AfterClass
   public static void teardown() throws IOException {
-    CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
-    cleaner.cleanupDataDirectories();
-    rmdir(TMP);
+    embedded.teardown();
   }
 
   @Before
@@ -135,42 +116,4 @@ public class CassandraClientTest {
     assertNotNull(config);
     assertTrue(config.length() > 0);
   }
-
-  private static void rmdir(String dir) throws IOException {
-    File dirFile = new File(dir);
-    if (dirFile.exists()) {
-      FileUtils.deleteDir(new File(dir));
-    }
-  }
-  /**
-   * Copies a resource from within the jar to a directory.
-   *
-   * @param resourceName
-   * @param directory
-   * @throws IOException
-   */
-  private static void copy(String resource, String directory) throws IOException {
-    mkdir(directory);
-    InputStream is = CassandraClientTest.class.getResourceAsStream(resource);
-    String fileName = resource.substring(resource.lastIndexOf("/") + 1);
-    File file = new File(directory + System.getProperty("file.separator") + fileName);
-    OutputStream out = new FileOutputStream(file);
-    byte buf[] = new byte[1024];
-    int len;
-    while ((len = is.read(buf)) > 0) {
-      out.write(buf, 0, len);
-    }
-    out.close();
-    is.close();
-  }
-
-  /**
-   * Creates a directory
-   * @param dir
-   * @throws IOException
-   */
-  private static void mkdir(String dir) throws IOException {
-    FileUtils.createDirectory(dir);
-  }
-
 }
