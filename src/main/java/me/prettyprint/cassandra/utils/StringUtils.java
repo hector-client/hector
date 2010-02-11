6867617044ba62477b630c14ee49143e1df23e19
Ran Tavory <rantav@gmail.com>
2010-02-11 12:35:56 +0200

    Add StringUtils class for easy string encoding/decoding

---
+++ src/main/java/me/prettyprint/cassandra/utils/StringUtils.java
@@ -0,0 +1,49 @@
+package me.prettyprint.cassandra.utils;
+
+import java.io.UnsupportedEncodingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Encoding and decoding utilities.
+ *
+ * @author Ran Tavory (rantav@gmail.com)
+ *
+ */
+public class StringUtils {
+
+  private static final Logger log = LoggerFactory.getLogger(StringUtils.class);
+
+  public static final String ENCODING = "utf-8";
+
+  /**
+   * Gets UTF-8 bytes from the string.
+   *
+   * @param s
+   * @return
+   */
+  public static byte[] bytes(String s) {
+    try {
+      return s.getBytes(ENCODING);
+    } catch (UnsupportedEncodingException e) {
+      log.error("UnsupportedEncodingException ", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Utility for converting bytes to strings. UTF-8 is assumed.
+   * @param bytes
+   * @return
+   */
+  public static String string(byte[] bytes) {
+    try {
+      return new String(bytes, ENCODING);
+    } catch (UnsupportedEncodingException e) {
+      log.error("UnsupportedEncodingException ", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+}
