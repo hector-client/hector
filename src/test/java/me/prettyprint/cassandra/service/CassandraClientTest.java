b8db8930f272f7e38112d5dbf979663f94d7626d
Ran Tavory <rantav@gmail.com>
2010-02-11 08:08:00 +0200

    Add a basic test case, that just fails

---
+++ src/test/java/me/prettyprint/cassandra/service/CassandraClientTest.java
@@ -0,0 +1,44 @@
+package me.prettyprint.cassandra.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CassandraClientTest {
+
+  @Test
+  public void testGetKeySpaceString() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetKeySpaceStringConsistencyLevel() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetStringProperty() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetKeyspaces() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetClusterName() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetTokenMap() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetConfigFile() {
+    fail("Not yet implemented");
+  }
+
+}
