a091f1893d4697f8ff1eb52a95cb226a654b908a
Arin Sarkissian <arin@digg.com>
2010-03-18 15:34:18 -0700

    start working on the tests

--- src/test/java/org/apache/cassandra/service/EmbeddedCassandraService.java
+++ src/test/java/org/apache/cassandra/service/EmbeddedCassandraService.java
@@ -1,4 +1,25 @@
 package org.apache.cassandra.service;
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 
 import org.apache.cassandra.thrift.CassandraDaemon;
 import org.apache.thrift.transport.TTransportException;
@@ -34,16 +55,19 @@ import java.io.IOException;
  * @author Ran Tavory (rantav@gmail.com)
  *
  */
-public class EmbeddedCassandraService implements Runnable {
+public class EmbeddedCassandraService implements Runnable
+{
 
-  CassandraDaemon cassandraDaemon;
+    CassandraDaemon cassandraDaemon;
 
-  public void init() throws TTransportException, IOException {
-    cassandraDaemon = new CassandraDaemon();
-    cassandraDaemon.init(null);
-  }
+    public void init() throws TTransportException, IOException
+    {
+        cassandraDaemon = new CassandraDaemon();
+        cassandraDaemon.init(null);
+    }
 
-  public void run() {
-    cassandraDaemon.start();
-  }
+    public void run()
+    {
+        cassandraDaemon.start();
+    }
 }
