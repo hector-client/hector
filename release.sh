c1cfd67dad1863e8ab622f537fb4e766450b8b2c
Ran Tavory <rantav@gmail.com>
2010-03-23 11:19:44 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Exit on error
+set -e
+# Require variable declaration
+set -u
+
+echo Running mvn install
+#mvn install
+
+# read the version from pom.xml
+version=$(sed -n "s/<version>\(.*\)<\/version>/\1/p" pom.xml | head -1)
+# remove whitespace
+version=$(echo $version)
+
+echo Version is: $version
+
+echo Copying artifacts
+target="releases/hector-$version"
+rm -rf $target*
+mkdir -p $target
+cp target/hector-$version* $target
+
+echo Copying lib jars
+cp lib/* $target
+cp antlib/* $target
+
+echo Zipping
+pushd releases
+zip -Tr hector-$version.zip hector-$version/
+popd
+
+echo DONE
