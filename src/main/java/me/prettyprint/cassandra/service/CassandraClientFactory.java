06fa6882cdd7eb919b2b0b06da50a365fcb66d1c
Ran Tavory <rantav@gmail.com>
2010-04-08 13:34:48 +0300

    Improve logging

--- src/main/java/me/prettyprint/cassandra/service/CassandraClientFactory.java
+++ src/main/java/me/prettyprint/cassandra/service/CassandraClientFactory.java
@@ -68,6 +68,7 @@ import org.slf4j.LoggerFactory;
 
   private Cassandra.Client createThriftClient(String  url, int port)
       throws TTransportException , TException {
+    log.debug("Creating a new thrift connection to {}:{}", url, port);
     TTransport tr = new TSocket(url, port, timeout);
     TProtocol proto = new TBinaryProtocol(tr);
     Cassandra.Client client = new Cassandra.Client(proto);
@@ -89,7 +90,7 @@ import org.slf4j.LoggerFactory;
    * if it was greater than zero. Otherwise look for an environment
    * variable name CASSANDRA_THRIFT_SOCKET_TIMEOUT value.
    * If doesn't exist, returns 0.
-   * @param cassandraHost 
+   * @param cassandraHost
    */
   private int getTimeout(CassandraHost cassandraHost) {
     int timeoutVar = 0;
@@ -139,6 +140,7 @@ import org.slf4j.LoggerFactory;
   }
 
   private void closeClient(CassandraClient cclient) {
+    log.debug("Closing client {}", cclient);
     ((CassandraClientPoolImpl) pool).reportDestroyed(cclient);
     Cassandra.Client client = cclient.getCassandra();
     client.getInputProtocol().getTransport().close();
