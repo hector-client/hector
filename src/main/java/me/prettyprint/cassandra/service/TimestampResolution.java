57f27fddc660594cbfa7bdfe8de890ee679a7ca5
Ran Tavory <rantav@gmail.com>
2010-05-11 10:09:09 +0300

    Improve docs

--- src/main/java/me/prettyprint/cassandra/service/TimestampResolution.java
+++ src/main/java/me/prettyprint/cassandra/service/TimestampResolution.java
@@ -2,10 +2,10 @@ package me.prettyprint.cassandra.service;
 
 /**
  * Resolution used to create timestamps.
- * Clients may with to use millisec, micro or sec, depending on the application
- * needs and existing data and other tools, so Hector wants to make that
+ * Clients may wish to use millisec, micro or sec, depending on the application
+ * needs and existing data and other tools, so Hector makes that
  * configurable.
- * @author Ran
+ * @author Ran Tavory (rantav@gmail.com)
  *
  */
 public enum TimestampResolution {
