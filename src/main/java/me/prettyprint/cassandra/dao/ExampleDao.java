f9382861162cea6991aa90a7a63248d86040e8a2
Ran Tavory <rantav@gmail.com>
2010-06-15 22:14:23 +0300

    Change ExampleDao to work with new exceptions heirarchy

--- src/main/java/me/prettyprint/cassandra/dao/ExampleDao.java
+++ src/main/java/me/prettyprint/cassandra/dao/ExampleDao.java
@@ -1,8 +1,9 @@
 package me.prettyprint.cassandra.dao;
 
+import me.prettyprint.cassandra.model.HectorException;
+import me.prettyprint.cassandra.model.NotFoundException;
 import me.prettyprint.cassandra.service.Keyspace;
 import org.apache.cassandra.thrift.ColumnPath;
-import org.apache.cassandra.thrift.NotFoundException;
 
 import static me.prettyprint.cassandra.utils.StringUtils.bytes;
 import static me.prettyprint.cassandra.utils.StringUtils.string;
@@ -33,7 +34,7 @@ public class ExampleDao {
   /** Column name where values are stored */
   private final String COLUMN_NAME = "v";
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws HectorException {
     ExampleDao ed = new ExampleDao();
     ed.insert("key1", "value1");
 
@@ -46,9 +47,9 @@ public class ExampleDao {
    * @param key   Key for the value
    * @param value the String value to insert
    */
-  public void insert(final String key, final String value) throws Exception {
+  public void insert(final String key, final String value) throws HectorException {
     execute(new Command<Void>() {
-      public Void execute(final Keyspace ks) throws Exception {
+      public Void execute(final Keyspace ks) throws HectorException {
         ks.insert(key, createColumnPath(COLUMN_NAME), bytes(value));
         return null;
       }
@@ -60,9 +61,9 @@ public class ExampleDao {
    *
    * @return The string value; null if no value exists for the given key.
    */
-  public String get(final String key) throws Exception {
+  public String get(final String key) throws HectorException {
     return execute(new Command<String>() {
-      public String execute(final Keyspace ks) throws Exception {
+      public String execute(final Keyspace ks) throws HectorException {
         try {
           return string(ks.getColumn(key, createColumnPath(COLUMN_NAME)).getValue());
         } catch (NotFoundException e) {
@@ -75,16 +76,16 @@ public class ExampleDao {
   /**
    * Delete a key from cassandra
    */
-  public void delete(final String key) throws Exception {
+  public void delete(final String key) throws HectorException {
     execute(new Command<Void>() {
-      public Void execute(final Keyspace ks) throws Exception {
+      public Void execute(final Keyspace ks) throws HectorException {
         ks.remove(key, createColumnPath(COLUMN_NAME));
         return null;
       }
     });
   }
 
-  protected static <T> T execute(Command<T> command) throws Exception {
+  protected static <T> T execute(Command<T> command) throws HectorException {
     return command.execute(CASSANDRA_HOST, CASSANDRA_PORT, CASSANDRA_KEYSPACE);
   }
 
--- src/main/java/me/prettyprint/cassandra/model/TransportException.java
+++
@@ -1,5 +0,0 @@
-package me.prettyprint.cassandra.model;
-
-public class TransportException extends HectorException {
-
-}
