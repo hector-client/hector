ccdca7e4b5a906f1034a342bdd51cb32ae9a0793
Ran Tavory <rantav@gmail.com>
2010-06-19 00:29:28 +0300

    Make AbstractSliceQuery extend a Query, as it should have

--- src/main/java/me/prettyprint/cassandra/model/AbstractSliceQuery.java
+++ src/main/java/me/prettyprint/cassandra/model/AbstractSliceQuery.java
@@ -3,7 +3,7 @@ package me.prettyprint.cassandra.model;
 import java.util.List;
 
 // base type for SliceQuery, RangeSliceQuery and MultigetSliceQuery
-/*package*/ interface AbstractSliceQuery {
+/*package*/ interface AbstractSliceQuery extends Query {
   SliceQuery setPredicate(List<Object> columnNames);
   SliceQuery setPredicate(Object start, Object finish, boolean reversed, int count);
 
