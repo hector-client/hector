046ba0dcde1c4f00cf55d4edae996b13f1b3a9c2
zznate <nate@vervewireless.com>
2010-06-18 21:17:11 -0700

    added a few method stubs, some comments

--- src/main/java/me/prettyprint/cassandra/model/Cluster.java
+++ src/main/java/me/prettyprint/cassandra/model/Cluster.java
@@ -1,5 +1,15 @@
 package me.prettyprint.cassandra.model;
 
-public interface Cluster {
+import java.util.List;
 
+import me.prettyprint.cassandra.service.CassandraHost;
+
+public interface Cluster {
+  
+  List<CassandraHost> getKnownHosts();
+  
+  void addHost(CassandraHost cassandraHost);
+  
+  // rest of the methods from the current CassandraCluster
+  
 }
--- src/main/java/me/prettyprint/cassandra/model/ClusterFactory.java
+++ src/main/java/me/prettyprint/cassandra/model/ClusterFactory.java
@@ -1,10 +1,20 @@
 package me.prettyprint.cassandra.model;
 
+import me.prettyprint.cassandra.service.CassandraHostConfigurator;
+
 public class ClusterFactory {
 
+  
   public static Cluster create(String hostIp) {
-    // TODO Auto-generated method stub
-    return null;
+    /*
+     I would like to move off of string literals for hosts, perhaps
+     providing them for convinience, and used specific types
+      
+     */
+    return create(new CassandraHostConfigurator(hostIp));
   }
 
+  public static Cluster create(CassandraHostConfigurator cassandraHostConfigurator) {
+    return null;
+  }
 }
