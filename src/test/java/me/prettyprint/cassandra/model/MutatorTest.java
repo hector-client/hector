bc1b7fbd25c243cfa45939fbb0926b9cd37b31ce
zznate <nate@vervewireless.com>
2010-07-05 22:50:52 -0700

    removed ignore on testInsert, added private row assertion helper

--- src/test/java/me/prettyprint/cassandra/model/MutatorTest.java
+++ src/test/java/me/prettyprint/cassandra/model/MutatorTest.java
@@ -5,13 +5,16 @@ import static me.prettyprint.cassandra.model.HFactory.createKeyspaceOperator;
 import static me.prettyprint.cassandra.model.HFactory.createMutator;
 import static me.prettyprint.cassandra.model.HFactory.getOrCreateCluster;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import me.prettyprint.cassandra.BaseEmbededServerSetupTest;
 import me.prettyprint.cassandra.extractors.StringExtractor;
+import me.prettyprint.cassandra.service.CassandraClient;
 import me.prettyprint.cassandra.service.Cluster;
+import me.prettyprint.cassandra.utils.StringUtils;
 
+import org.apache.cassandra.thrift.ColumnPath;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 public class MutatorTest extends BaseEmbededServerSetupTest {
 
@@ -27,13 +30,11 @@ public class MutatorTest extends BaseEmbededServerSetupTest {
   }
 
   @Test
-  @Ignore
-  public void testInsert() {
-    /*
-    Mutator m = MutatorFactory.createMutator(keyspaceOperator);
-    MutationResult mr = m.insert("testInsertGetRemove", "Standard1",
-        m.createColumn("testInsertGetRemove", "testInsertGetRemove_value_"));
-     */
+  public void testInsert() {    
+    Mutator m = createMutator(keyspaceOperator);
+    MutationResult mr = m.insert("k", "Standard1", createColumn("name", "value", se, se));
+    assertTrue("Execution time on single insert should be > 0",mr.getExecutionTimeMicro() > 0);
+    assertColumnExists("Keyspace1", "Standard1", "k", "name");
   }
 
   @Test
@@ -47,6 +48,9 @@ public class MutatorTest extends BaseEmbededServerSetupTest {
     MutationResult r = m.execute();
     assertTrue("Execute time should be > 0", r.getExecutionTimeMicro() > 0);
 
+    for (int i = 0; i < 5; i++) {
+      assertColumnExists("Keyspace1", "Standard1", "k"+i, "name");
+    }
     // Execute an empty mutation
     r = m.execute();
     assertEquals("Execute time should be 0", 0, r.getExecutionTimeMicro());
@@ -64,6 +68,15 @@ public class MutatorTest extends BaseEmbededServerSetupTest {
       m.addDeletion("k" + i, cf, "name", se);
     }
     m.execute();
+  }
+  
+  private void assertColumnExists(String keyspace, String cf, String key, String column) {
+    ColumnPath cp = new ColumnPath(cf);
+    cp.setColumn(StringUtils.bytes(column));
+    CassandraClient client = cluster.borrowClient();
+    assertNotNull(String.format("Should have value for %s.%s[%s][%s]", new String[]{keyspace,cf,key,column}),
+        client.getKeyspace(keyspace).getColumn(key, cp));
+    cluster.releaseClient(client);
   }
 
 }
