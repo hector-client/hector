66a43cb191e98f80a54898ab95e07e4b29f50128
zznate <nate@vervewireless.com>
2010-07-05 23:59:19 -0700

    built out more of HSuperColumn

--- src/main/java/me/prettyprint/cassandra/model/HSuperColumn.java
+++ src/main/java/me/prettyprint/cassandra/model/HSuperColumn.java
@@ -9,15 +9,44 @@ import java.util.List;
  * @param <V> Column value type
  */
 public class HSuperColumn<SN,N,V> {
-
+  
+  private SN name;
   private List<HColumn<N,V>> columns;
+  private long timestamp;
+  private final Extractor<SN> nameExtractor;
+  
+  public HSuperColumn(SN sName, Extractor<SN> sNameExtractor, List<HColumn<N, V>> columns, long timestamp) {
+    this.name = sName;
+    this.nameExtractor = sNameExtractor;
+    this.columns = columns;
+    this.timestamp = timestamp;
+  }
+  
+  public HSuperColumn<SN, N, V> setName(SN name) {
+    this.name = name;
+    return this;
+  }
+  
+  public HSuperColumn<SN, N, V> setValues(List<HColumn<N, V>> values) {
+    this.columns = values;
+    return this;
+  }
+  
+  public HSuperColumn<SN, N, V> setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+    return this;
+  }
+  
+  public long getTimestamp() {
+    return timestamp;
+  }
   
   public int getSize() {
     return columns.size();
   }
   
   public SN getName() {
-    return null;
+    return name;
   }
   
   public List<HColumn<N,V>> getColumns() {
@@ -26,6 +55,14 @@ public class HSuperColumn<SN,N,V> {
   
   public HColumn<N,V> get(int i) {
     return columns.get(i);
+  }
+  
+  public Extractor<SN> getNameExtractor() {
+    return nameExtractor;
+  }
+
+  public byte[] getNameBytes() {
+    return nameExtractor.toBytes(getName());
   }
   
 }
