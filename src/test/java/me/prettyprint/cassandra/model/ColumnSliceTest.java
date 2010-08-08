b822d06eef366e224005ec64e70d46e3b801933c
Ran Tavory <rantav@gmail.com>
2010-08-08 21:24:20 +0300

    src/main/java/me/prettyprint/cassandra/extractors/
    Implement ColumnSliceTest and SuperColumnSliceTest

--- src/test/java/me/prettyprint/cassandra/model/ColumnSliceTest.java
+++ src/test/java/me/prettyprint/cassandra/model/ColumnSliceTest.java
@@ -1,15 +1,39 @@
 package me.prettyprint.cassandra.model;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+import me.prettyprint.cassandra.extractors.LongExtractor;
+import me.prettyprint.cassandra.extractors.StringExtractor;
+
+import org.apache.cassandra.thrift.Column;
 import org.junit.Test;
 
 
-
+/**
+ *
+ * @author Ran Tavory
+ *
+ */
 public class ColumnSliceTest {
 
+
   @Test
-  @Ignore
-  public void testTODO() {
-    //TODO
+  public void testConstruction() {
+    StringExtractor se = StringExtractor.get();
+    LongExtractor le = LongExtractor.get();
+    List<Column> tColumns = new ArrayList<Column>();
+    ColumnSlice<String, Long> slice = new ColumnSlice<String, Long>(tColumns, se, le);
+    Assert.assertTrue(slice.getColumns().isEmpty());
+
+    tColumns.add(new Column(new byte[]{}, new byte[]{}, 0));
+    slice = new ColumnSlice<String, Long>(tColumns, se, le);
+    Assert.assertEquals(1, slice.getColumns().size());
+
+    tColumns = new ArrayList<Column>();
+    tColumns.add(new Column(se.toBytes("1"), le.toBytes(1L), 0));
+    slice = new ColumnSlice<String, Long>(tColumns, se, le);
+    Assert.assertEquals((Long) 1L, slice.getColumnByName("1").getValue());
   }
 }
