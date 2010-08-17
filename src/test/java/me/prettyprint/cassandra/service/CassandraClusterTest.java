a6690fc7730411d9f35a0bf360c0abca026ff1fb
Ran Tavory <rantav@gmail.com>
2010-08-17 23:04:16 +0300

    Fix CassandraClusterTest to work with the new thrift version and new cassandra.yaml configuration

--- src/test/java/me/prettyprint/cassandra/service/CassandraClusterTest.java
+++ src/test/java/me/prettyprint/cassandra/service/CassandraClusterTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertNotNull;
 
 import java.net.UnknownHostException;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import me.prettyprint.cassandra.BaseEmbededServerSetupTest;
 
@@ -19,37 +17,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class CassandraClusterTest extends BaseEmbededServerSetupTest { 
-  
+public class CassandraClusterTest extends BaseEmbededServerSetupTest {
+
   private Cluster cassandraCluster;
   private CassandraHostConfigurator cassandraHostConfigurator;
 
-  
+
   @Before
   public void setupCase() throws TTransportException, TException, IllegalArgumentException,
           NotFoundException, UnknownHostException, Exception {
-    cassandraHostConfigurator = new CassandraHostConfigurator("localhost:9170");        
+    cassandraHostConfigurator = new CassandraHostConfigurator("localhost:9170");
     cassandraCluster = new Cluster("Test Cluster", cassandraHostConfigurator);
   }
-  
+
   @Test
   public void testDescribeKeyspaces() throws Exception {
     List<KsDef> keyspaces = cassandraCluster.describeKeyspaces();
     assertEquals(2,keyspaces.size());
   }
-  
+
   @Test
   public void testDescribeClusterName() throws Exception {
     assertEquals("Test Cluster",cassandraCluster.describeClusterName());
   }
-  
+
   /**
    * This will need to be updated as we update the Thrift API, but probably a good sanity check
-   * 
+   *
    */
   @Test
   public void testDescribeThriftVersion() throws Exception {
-    assertEquals("10.0.0",cassandraCluster.describeThriftVersion());
+    assertEquals("11.0.0",cassandraCluster.describeThriftVersion());
   }
 
   @Test
@@ -57,14 +55,14 @@ public class CassandraClusterTest extends BaseEmbededServerSetupTest {
     List<TokenRange> ring = cassandraCluster.describeRing("Keyspace1");
     assertEquals(1, ring.size());
   }
-  
-  
-  
+
+
+
   @Test
   public void testDescribeKeyspace() throws Exception {
     KsDef keyspaceDetail = cassandraCluster.describeKeyspace("Keyspace1");
     assertNotNull(keyspaceDetail);
-    assertEquals(6,keyspaceDetail.cf_defs.size());    
+    assertEquals(7, keyspaceDetail.cf_defs.size());
   }
-  
+
 }
