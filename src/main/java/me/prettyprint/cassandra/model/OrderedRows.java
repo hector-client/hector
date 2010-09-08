3ebab32ea87c9b4d9b3a05dad9bfd44ac62e1aa0
zznate <nate@vervewireless.com>
2010-09-08 12:51:58 -0500

    switched to arraydeque underneath OrderedRows for peekLast type of stuff

--- src/main/java/me/prettyprint/cassandra/model/OrderedRows.java
+++ src/main/java/me/prettyprint/cassandra/model/OrderedRows.java
@@ -1,5 +1,6 @@
 package me.prettyprint.cassandra.model;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -11,18 +12,19 @@ import org.apache.cassandra.thrift.Column;
 /**
  * Return type from get_range_slices for simple columns
  * @author Ran Tavory
+ * @author zznate
  *
  * @param <N>
  * @param <V>
  */
 public final class OrderedRows<K,N,V> extends Rows<K,N,V> {
 
-  private final List<Row<K,N,V>> rowsList;
+  private final ArrayDeque<Row<K,N,V>> rowsList;
 
   public OrderedRows(LinkedHashMap<K, List<Column>> thriftRet, Serializer<N> nameSerializer,
       Serializer<V> valueSerializer) {
     super(thriftRet, nameSerializer, valueSerializer);
-    rowsList = new ArrayList<Row<K,N,V>>(thriftRet.size());
+    rowsList = new ArrayDeque<Row<K,N,V>>(thriftRet.size());
     for (Map.Entry<K, List<Column>> entry: thriftRet.entrySet()) {
       rowsList.add(new Row<K,N,V>(entry.getKey(), entry.getValue(), nameSerializer, valueSerializer));
     }
@@ -33,6 +35,14 @@ public final class OrderedRows<K,N,V> extends Rows<K,N,V> {
    * @return an unmodifiable list of Rows
    */
   public List<Row<K,N,V>> getList() {
-    return Collections.unmodifiableList(rowsList);
+    return Collections.unmodifiableList(new ArrayList<Row<K, N, V>>(rowsList));
+  }
+  
+  /**
+   * Returns the last element in this row result. Helpful for passing along for paging situations.
+   * @return
+   */
+  public Row<K, N, V> peekLast() {
+    return rowsList != null && rowsList.size() > 0 ? rowsList.peekLast() : null;
   }
 }
