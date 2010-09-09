1e06a3163853d899a32451abc96b9ac9905da1b9
Bozhidar Bozhanov <Bozhidar Bozhanov@bozho.mshome.net>
2010-09-09 08:43:28 +0300

    Type-inferring serializer (forgot in previous commit)

---
+++ src/main/java/me/prettyprint/cassandra/serializers/TypeInferringSerializer.java
@@ -0,0 +1,34 @@
+package me.prettyprint.cassandra.serializers;
+
+import me.prettyprint.cassandra.model.Serializer;
+
+/**
+ * A serializer that dynamically delegates to a proper serializer based on the
+ * value passed
+ *
+ * @author Bozhidar Bozhanov
+ *
+ * @param <T> type
+ */
+public class TypeInferringSerializer<T> extends AbstractSerializer<T> implements
+    Serializer<T> {
+
+  @SuppressWarnings("rawtypes")
+  private static final TypeInferringSerializer instance = new TypeInferringSerializer();
+
+  @SuppressWarnings("unchecked")
+  public static <T> TypeInferringSerializer<T> get() {
+    return instance;
+  }
+
+  @Override
+  public byte[] toBytes(T obj) {
+    return SerializerTypeInferer.getSerializer(obj).toBytes(obj);
+  }
+
+  @Override
+  public T fromBytes(byte[] bytes) {
+    throw new IllegalStateException(
+        "The type inferrign serializer can only be used for data going to the database, and not data coming from the database");
+  }
+}
