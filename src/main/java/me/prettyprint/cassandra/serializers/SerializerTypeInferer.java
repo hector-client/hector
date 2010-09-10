60ba6d9d2135330248855f182e998828e416df40
Ran Tavory <rantav@gmail.com>
2010-09-10 14:51:32 +0300

    Add some type inference smartness for serializers. This was copied from 0.7.0 and pulled from http://github.com/rantav/hector/pull/58

---
+++ src/main/java/me/prettyprint/cassandra/serializers/SerializerTypeInferer.java
@@ -0,0 +1,38 @@
+package me.prettyprint.cassandra.serializers;
+
+import java.util.UUID;
+
+import me.prettyprint.cassandra.model.Serializer;
+import me.prettyprint.cassandra.serializers.BytesSerializer;
+import me.prettyprint.cassandra.serializers.LongSerializer;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.serializers.UUIDSerializer;
+
+/**
+ * Utility class that infers the concrete Serializer needed to turn a value into
+ * its binary representation
+ *
+ * @author Bozhidar Bozhanov
+ *
+ */
+public class SerializerTypeInferer {
+
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  public static <T> Serializer<T> getSerializer(Object value) {
+    Serializer serializer = null;
+    if (value instanceof UUID) {
+      serializer = UUIDSerializer.get();
+    } else if (value instanceof String) {
+      serializer = StringSerializer.get();
+    } else if (value instanceof Long) {
+      serializer = LongSerializer.get();
+    } else if (value instanceof byte[]) {
+      serializer = BytesSerializer.get();
+    } else {
+      serializer = ObjectSerializer.get();
+    }
+    // TODO other number serializers
+
+    return serializer;
+  }
+}
