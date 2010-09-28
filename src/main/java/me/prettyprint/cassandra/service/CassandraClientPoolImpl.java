b8d289ec77f34a9d5c09e026e34a4301f1be7f95
zznate <nate@vervewireless.com>
2010-09-27 23:56:05 -0500

    updated version in pom, added fix for CCP

--- src/main/java/me/prettyprint/cassandra/service/CassandraClientPoolImpl.java
+++ src/main/java/me/prettyprint/cassandra/service/CassandraClientPoolImpl.java
@@ -38,7 +38,7 @@ import org.slf4j.LoggerFactory;
   private final CassandraClientMonitor clientMonitor;
 
   private CassandraHostConfigurator cassandraHostConfigurator;
-  private Cluster cluster;
+  private final Cluster cluster;
 
   public CassandraClientPoolImpl(CassandraClientMonitor clientMonitor) {
     log.info("Creating a CassandraClientPool");
@@ -55,14 +55,12 @@ import org.slf4j.LoggerFactory;
       log.debug("Maybe creating pool-by-host instance for {} at {}", cassandraHost, this);
       getPool(cassandraHost);
     }
-    this.cluster = new ThriftCluster("Default Cluster", this);
   }
 
   public CassandraClientPoolImpl(CassandraClientMonitor clientMonitor,
       CassandraHostConfigurator cassandraHostConfigurator) {
     this(clientMonitor, cassandraHostConfigurator.buildCassandraHosts());
     this.cassandraHostConfigurator = cassandraHostConfigurator;
-    this.cluster = new ThriftCluster("Default Cluster", this);
   }
 
 
