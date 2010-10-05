4e032f45eedcf5ec5fb3ccefa990a6b0dcd74830
zznate <nate@vervewireless.com>
2010-10-04 22:32:25 -0500

    added additional index clause arguments

--- src/test/java/me/prettyprint/cassandra/model/IndexedSlicesQueryTest.java
+++ src/test/java/me/prettyprint/cassandra/model/IndexedSlicesQueryTest.java
@@ -24,11 +24,26 @@ public class IndexedSlicesQueryTest extends BaseEmbededServerSetupTest {
   private static final LongSerializer le = new LongSerializer();
   private Cluster cluster;
   private Keyspace keyspace;
+  private String cf = "Indexed1";
 
   @Before
   public void setupCase() {
     cluster = getOrCreateCluster("MyCluster", "127.0.0.1:9170");
     keyspace = createKeyspace(KEYSPACE, cluster);
+    createMutator(keyspace, se)
+    .addInsertion("indexedSlicesTest_key1", cf, createColumn("birthyear", 1974L, se, le))
+    .addInsertion("indexedSlicesTest_key1", cf, createColumn("birthmonth", 4L, se, le))
+    .addInsertion("indexedSlicesTest_key2", cf, createColumn("birthyear", 1975L, se, le))
+    .addInsertion("indexedSlicesTest_key2", cf, createColumn("birthmonth", 4L, se, le))
+    .addInsertion("indexedSlicesTest_key3", cf, createColumn("birthyear", 1975L, se, le))
+    .addInsertion("indexedSlicesTest_key3", cf, createColumn("birthmonth", 5L, se, le))
+    .addInsertion("indexedSlicesTest_key4", cf, createColumn("birthyear", 1975L, se, le))
+    .addInsertion("indexedSlicesTest_key4", cf, createColumn("birthmonth", 6L, se, le))
+    .addInsertion("indexedSlicesTest_key5", cf, createColumn("birthyear", 1975L, se, le))
+    .addInsertion("indexedSlicesTest_key5", cf, createColumn("birthmonth", 7L, se, le))
+    .addInsertion("indexedSlicesTest_key6", cf, createColumn("birthyear", 1976L, se, le))
+    .addInsertion("indexedSlicesTest_key6", cf, createColumn("birthmonth", 6L, se, le))
+    .execute();
   }
 
   @After
@@ -38,24 +53,31 @@ public class IndexedSlicesQueryTest extends BaseEmbededServerSetupTest {
   }
 
   @Test
-  public void testInsertGetRemove() {
-    String cf = "Indexed1";
-
-    createMutator(keyspace, se)
-      .addInsertion("indexedSlicesTest_key1", cf, createColumn("birthdate", 1L, se, le))
-      .addInsertion("indexedSlicesTest_key2", cf, createColumn("birthdate", 2L, se, le))
-      .addInsertion("indexedSlicesTest_key3", cf, createColumn("birthdate", 3L, se, le))
-      .addInsertion("indexedSlicesTest_key4", cf, createColumn("birthdate", 4L, se, le))
-      .addInsertion("indexedSlicesTest_key5", cf, createColumn("birthdate", 5L, se, le))
-      .execute();
+  public void testInsertGetRemove() {        
 
     IndexedSlicesQuery<String, String, Long> indexedSlicesQuery = new IndexedSlicesQuery<String, String, Long>(keyspace, se, se, le);
-    indexedSlicesQuery.addEqualsExpression("birthdate", 4L);
-    indexedSlicesQuery.setColumnNames("birthdate");
+    indexedSlicesQuery.addEqualsExpression("birthyear", 1975L);
+    indexedSlicesQuery.setColumnNames("birthyear");
     indexedSlicesQuery.setColumnFamily(cf);
     indexedSlicesQuery.setStartKey("");
     QueryResult<OrderedRows<String, String, Long>> result = indexedSlicesQuery.execute();
-    assertEquals(1, result.get().getList().size());
+    assertEquals(4, result.get().getList().size());
+
+
+  }
+  
+  @Test
+  public void testMultiClause() {        
+
+    IndexedSlicesQuery<String, String, Long> indexedSlicesQuery = new IndexedSlicesQuery<String, String, Long>(keyspace, se, se, le);
+    indexedSlicesQuery.addEqualsExpression("birthyear", 1975L);
+    indexedSlicesQuery.addGteExpression("birthmonth", 4L);
+    indexedSlicesQuery.addLteExpression("birthmonth", 6L);
+    indexedSlicesQuery.setColumnNames("birthyear");
+    indexedSlicesQuery.setColumnFamily(cf);
+    indexedSlicesQuery.setStartKey("");
+    QueryResult<OrderedRows<String, String, Long>> result = indexedSlicesQuery.execute();
+    assertEquals(3, result.get().getList().size());
 
 
   }
