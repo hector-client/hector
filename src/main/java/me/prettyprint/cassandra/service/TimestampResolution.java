0888646cc21d7b665ecac78480844230fb3a3aba
Ran Tavory <rantav@gmail.com>
2010-10-07 21:57:20 +0200

    Improve Clock Resolution kudos to jancona and patricioe

--- src/main/java/me/prettyprint/cassandra/service/TimestampResolution.java
+++ src/main/java/me/prettyprint/cassandra/service/TimestampResolution.java
@@ -1,5 +1,7 @@
 package me.prettyprint.cassandra.service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Resolution used to create timestamps.
  * Clients may wish to use millisec, micro or sec, depending on the application
@@ -11,16 +13,30 @@ package me.prettyprint.cassandra.service;
 public enum TimestampResolution {
   SECONDS, MILLISECONDS, MICROSECONDS;
   
+  /** The last time value issued. Used to try to prevent duplicates. */
+  private static final AtomicLong lastTime = new AtomicLong(Long.MIN_VALUE);
+
   public long createTimestamp() {
     long current = System.currentTimeMillis();
-    switch(this) {
+    switch (this) {
     case MICROSECONDS:
-      return current * 1000;
+      // The following simmulates a microsec resolution by advancing a static counter every time
+      // a client calls the createClock method, simulating a tick.
+      long us = current * 1000;
+      if (us > lastTime.longValue()) {
+        lastTime.set(us);
+      } else { // the time i got from the system is equals or less (hope not - clock going
+               // backwards)
+        // One more "microsecond"
+        us = lastTime.incrementAndGet();
+      }
+      return us;
     case MILLISECONDS:
       return current;
     case SECONDS:
       return current / 1000;
-    };
+    }
+    ;
     return current;
   }
 }
