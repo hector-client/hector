96414e195a81de411fd43466eabfe462d3335791
patricioe <patricioe@gmail.com>
2010-11-08 20:23:27 -0800

    Add MICROSECONDS_SYNC and keep MICROSECONDS (not synchronized)
    Add fully synchronized MICROSECONDS_SYNC to provide unique time across and within threads.

--- src/main/java/me/prettyprint/cassandra/service/ClockResolution.java
+++ src/main/java/me/prettyprint/cassandra/service/ClockResolution.java
@@ -4,38 +4,54 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Resolution used to create clocks.
- * Clients may wish to use millisec, micro or sec, depending on the application
- * needs and existing data and other tools, so Hector makes that
+ * Clients may wish to use seconds, milliseconds, microseconds or synchronized microseconds,
+ * depending on the application needs and existing data and other tools, so Hector makes that
  * configurable.
- * @author Ran Tavory (rantav@gmail.com)
+ * Synchronized microseconds(MICROSECONDS_SYNC) guarantees unique clock within and across threads.
  *
+ * @author Ran Tavory (rantav@gmail.com)
+ * @author Patricio Echague (pechague@gmail.com)
  */
 public enum ClockResolution {
-  SECONDS, MILLISECONDS, MICROSECONDS;
-  /** The last time value issued. Used to try to prevent duplicates. */
-  private static final AtomicLong lastTime = new AtomicLong(Long.MIN_VALUE);
+  SECONDS, MILLISECONDS, MICROSECONDS, MICROSECONDS_SYNC;
+
+  private static final long ONE_THOUSAND = 1000L;
+
+  /** The last time value issued. Used to try to prevent duplicates. Valid only with MICROSECONDS_SYNC*/
+  private static long lastTime = -1;
+
+  static {
+	  synchronized (ClockResolution.class) {
+		  lastTime = System.currentTimeMillis() * ONE_THOUSAND;
+	}
+  }
 
   public long createClock() {
-    long current = System.currentTimeMillis();
     switch (this) {
-    case MICROSECONDS:
-      // The following simmulates a microsec resolution by advancing a static counter every time
-      // a client calls the createClock method, simulating a tick.
-      long us = current * 1000;
-      if (us > lastTime.longValue()) {
-        lastTime.set(us);
-      } else { // the time i got from the system is equals or less (hope not - clock going
-               // backwards)
-        // One more "microsecond"
-        us = lastTime.incrementAndGet();
-      }
-      return us;
-    case MILLISECONDS:
-      return current;
-    case SECONDS:
-      return current / 1000;
-    }
-    ;
-    return current;
+	    case MICROSECONDS:
+	    	return System.currentTimeMillis() * ONE_THOUSAND;
+	    case MICROSECONDS_SYNC:
+	      // The following simulates a microseconds resolution by advancing a static counter
+	      // every time a client calls the createClock method, simulating a tick.
+	      long us = System.currentTimeMillis() * ONE_THOUSAND;
+	      // Synchronized to guarantee unique time within and across threads.
+	      synchronized (ClockResolution.class) {
+	         if (us > lastTime) {
+	        	 lastTime = us;
+	         } else {
+	        	 // the time i got from the system is equals or less
+	        	 // (hope not - clock going backwards)
+	        	 // One more "microsecond"
+	        	 us = lastTime++;
+	         }
+	      }
+	      return us;
+	    case MILLISECONDS:
+	      return System.currentTimeMillis();
+	    case SECONDS:
+	      return System.currentTimeMillis() / 1000;
+	}
+
+    return System.currentTimeMillis();
   }
 }
