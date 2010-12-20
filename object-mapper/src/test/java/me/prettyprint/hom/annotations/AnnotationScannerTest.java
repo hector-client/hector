bd6531e3806e5e09f03599374ee171e3b91c03f1
zznate <nate@vervewireless.com>
2010-12-20 14:38:07 -0600

    updated formatting, mycompany annotations

--- object-mapper/src/test/java/me/prettyprint/hom/annotations/AnnotationScannerTest.java
+++ object-mapper/src/test/java/me/prettyprint/hom/annotations/AnnotationScannerTest.java
@@ -18,24 +18,24 @@ import org.junit.Test;
 
 public class AnnotationScannerTest {
 
-    @Test
-    public void testScanForAnnotation() {
-        AnnotationScanner scanner = new AnnotationScanner();
-        Set<Class<?>> classSet = scanner.scan("me.prettyprint.hom.beans", Entity.class);
+  @Test
+  public void testScanForAnnotation() {
+    AnnotationScanner scanner = new AnnotationScanner();
+    Set<Class<?>> classSet = scanner.scan("me.prettyprint.hom.beans", Entity.class);
 
-        int count = 0;
-        assertTrue(classSet.contains(MyTestBean.class));
-        count++;
-        assertTrue(classSet.contains(MyBlueTestBean.class));
-        count++;
-        assertTrue(classSet.contains(MyRedTestBean.class));
-        count++;
-        assertTrue(classSet.contains(MyPurpleTestBean.class));
-        count++;
-        assertTrue(classSet.contains(MyCustomIdBean.class));
-        count++;
+    int count = 0;
+    assertTrue(classSet.contains(MyTestBean.class));
+    count++;
+    assertTrue(classSet.contains(MyBlueTestBean.class));
+    count++;
+    assertTrue(classSet.contains(MyRedTestBean.class));
+    count++;
+    assertTrue(classSet.contains(MyPurpleTestBean.class));
+    count++;
+    assertTrue(classSet.contains(MyCustomIdBean.class));
+    count++;
 
-        assertEquals(count, classSet.size());
-    }
+    assertEquals(count, classSet.size());
+  }
 
 }
