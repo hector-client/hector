c4fc0180beaf96e435b8d607eed3fbaec2c7a8da
zznate <nate@vervewireless.com>
2010-12-20 14:25:12 -0600

    updated formatting

--- object-mapper/src/main/java/me/prettyprint/hom/EntityManager.java
+++ object-mapper/src/main/java/me/prettyprint/hom/EntityManager.java
@@ -19,148 +19,148 @@ import org.slf4j.LoggerFactory;
  * @author
  */
 public class EntityManager {
-    private static Logger logger = LoggerFactory.getLogger(EntityManager.class);
+  private static Logger logger = LoggerFactory.getLogger(EntityManager.class);
 
-    private Keyspace keyspace;
-    private HectorObjectMapper objMapper;
-    private ClassCacheMgr cacheMgr;
+  private Keyspace keyspace;
+  private HectorObjectMapper objMapper;
+  private ClassCacheMgr cacheMgr;
 
-    public EntityManager(Keyspace keyspace, String classpathPrefix) {
-        this(keyspace, new String[] {
-            classpathPrefix }, null, null);
+  public EntityManager(Keyspace keyspace, String classpathPrefix) {
+    this(keyspace, new String[] {
+        classpathPrefix }, null, null);
+  }
+
+  public EntityManager(Keyspace keyspace, String[] classpathPrefix) {
+    this(keyspace, classpathPrefix, null, null);
+  }
+
+  public EntityManager(Keyspace keyspace, String[] classpathPrefix, ClassCacheMgr cacheMgr,
+      HectorObjectMapper objMapper) {
+    this.keyspace = keyspace;
+
+    if (null != cacheMgr) {
+      this.cacheMgr = cacheMgr;
+    }
+    else {
+      this.cacheMgr = new ClassCacheMgr();
     }
 
-    public EntityManager(Keyspace keyspace, String[] classpathPrefix) {
-        this(keyspace, classpathPrefix, null, null);
+    if (null != objMapper) {
+      this.objMapper = objMapper;
+    }
+    else {
+      this.objMapper = new HectorObjectMapper(this.cacheMgr);
     }
 
-    public EntityManager(Keyspace keyspace, String[] classpathPrefix, ClassCacheMgr cacheMgr,
-            HectorObjectMapper objMapper) {
-        this.keyspace = keyspace;
+    initialize(classpathPrefix);
+  }
 
-        if (null != cacheMgr) {
-            this.cacheMgr = cacheMgr;
-        }
-        else {
-            this.cacheMgr = new ClassCacheMgr();
-        }
+  /**
+   * Initialize the manager by scanning the classpath starting with the
+   * <code>classpathPrefix</code>, looking for classes annotated with
+   * {@link Entity}. If an Entity class is found, it looks for the
+   * {@link Table} annotation to determine the Cassandra column family name.
+   * 
+   * @param classpathPrefixArr
+   */
+  public void initialize(String[] classpathPrefixArr) {
+    if (null != classpathPrefixArr && 0 < classpathPrefixArr.length) {
+      logger.debug("classpath array has " + classpathPrefixArr.length + " packages");
+      for (String classpathPrefix : classpathPrefixArr) {
+        logger.debug("classpath array : " + classpathPrefix);
+        initializeClasspath(classpathPrefix);
+      }
+    }
+    else {
+      logger.debug("classpath array is null or empty : "
+          + (null != classpathPrefixArr ? classpathPrefixArr.length : "null"));
+    }
+  }
 
-        if (null != objMapper) {
-            this.objMapper = objMapper;
-        }
-        else {
-            this.objMapper = new HectorObjectMapper(this.cacheMgr);
-        }
+  private void initializeClasspath(String classpathPrefix) {
+    AnnotationScanner scanner = new AnnotationScanner();
+    Set<Class<?>> classSet = scanner.scan(classpathPrefix, Entity.class);
+    for (Class<?> clazz : classSet) {
+      cacheMgr.initializeCacheForClass(clazz);
+    }
+  }
 
-        initialize(classpathPrefix);
+  /**
+   * Load an entity instance. If the ID does not map to a persisted entity,
+   * then null is returned.
+   * 
+   * @param <T>
+   *            The type of entity to load for compile time type checking
+   * @param <I>
+   *            Type of the entity's ID
+   * @param clazz
+   *            The type of entity to load for runtime instance creation
+   * @param id
+   *            ID of the instance to load
+   * @return instance of Entity or null if can't be found
+   */
+  public <T, I> T load(Class<T> clazz, I id) {
+    if (null == clazz) {
+      throw new IllegalArgumentException("clazz cannot be null");
+    }
+    if (null == id) {
+      throw new IllegalArgumentException("id cannot be null");
     }
 
-    /**
-     * Initialize the manager by scanning the classpath starting with the
-     * <code>classpathPrefix</code>, looking for classes annotated with
-     * {@link Entity}. If an Entity class is found, it looks for the
-     * {@link Table} annotation to determine the Cassandra column family name.
-     * 
-     * @param classpathPrefixArr
-     */
-    public void initialize(String[] classpathPrefixArr) {
-        if (null != classpathPrefixArr && 0 < classpathPrefixArr.length) {
-            logger.debug("classpath array has " + classpathPrefixArr.length + " packages");
-            for (String classpathPrefix : classpathPrefixArr) {
-                logger.debug("classpath array : " + classpathPrefix);
-                initializeClasspath(classpathPrefix);
-            }
-        }
-        else {
-            logger.debug("classpath array is null or empty : "
-                    + (null != classpathPrefixArr ? classpathPrefixArr.length : "null"));
-        }
+    CFMappingDef<T, I> cfMapDef = cacheMgr.getCfMapDef(clazz, false);
+    if (null == cfMapDef) {
+      throw new IllegalStateException("No class annotated with @Entity for type, " + clazz.getName());
     }
 
-    private void initializeClasspath(String classpathPrefix) {
-        AnnotationScanner scanner = new AnnotationScanner();
-        Set<Class<?>> classSet = scanner.scan(classpathPrefix, Entity.class);
-        for (Class<?> clazz : classSet) {
-            cacheMgr.initializeCacheForClass(clazz);
-        }
+    return objMapper.getObject(keyspace, cfMapDef.getColFamName(), clazz, id);
+  }
+
+  /**
+   * Load an entity instance given the raw column slice. This is a stop gap
+   * solution for instanting objects using entity manager while iterating over
+   * rows.
+   * 
+   * @param <T>
+   *            The type of entity to load for compile time type checking
+   * @param <I>
+   *            Type of the entity's ID
+   * @param clazz
+   *            The type of entity to load for runtime instance creation
+   * @param id
+   *            ID of the instance to load
+   * @param rowSlice
+   *            Raw row slice as returned from Hector API, of the type
+   *            <code>ColumnSlice<String, byte[]></code>
+   * @return
+   */
+  public <T, I> T load(Class<T> clazz, I id, ColumnSlice<String, byte[]> colSlice) {
+    if (null == clazz) {
+      throw new IllegalArgumentException("clazz cannot be null");
+    }
+    if (null == id) {
+      throw new IllegalArgumentException("id cannot be null");
     }
 
-    /**
-     * Load an entity instance. If the ID does not map to a persisted entity,
-     * then null is returned.
-     * 
-     * @param <T>
-     *            The type of entity to load for compile time type checking
-     * @param <I>
-     *            Type of the entity's ID
-     * @param clazz
-     *            The type of entity to load for runtime instance creation
-     * @param id
-     *            ID of the instance to load
-     * @return instance of Entity or null if can't be found
-     */
-    public <T, I> T load(Class<T> clazz, I id) {
-        if (null == clazz) {
-            throw new IllegalArgumentException("clazz cannot be null");
-        }
-        if (null == id) {
-            throw new IllegalArgumentException("id cannot be null");
-        }
-
-        CFMappingDef<T, I> cfMapDef = cacheMgr.getCfMapDef(clazz, false);
-        if (null == cfMapDef) {
-            throw new IllegalStateException("No class annotated with @Entity for type, " + clazz.getName());
-        }
-
-        return objMapper.getObject(keyspace, cfMapDef.getColFamName(), clazz, id);
+    CFMappingDef<T, I> cfMapDef = cacheMgr.getCfMapDef(clazz, false);
+    if (null == cfMapDef) {
+      throw new IllegalStateException("No class annotated with @Entity for type, " + clazz.getName());
     }
 
-    /**
-     * Load an entity instance given the raw column slice. This is a stop gap
-     * solution for instanting objects using entity manager while iterating over
-     * rows.
-     * 
-     * @param <T>
-     *            The type of entity to load for compile time type checking
-     * @param <I>
-     *            Type of the entity's ID
-     * @param clazz
-     *            The type of entity to load for runtime instance creation
-     * @param id
-     *            ID of the instance to load
-     * @param rowSlice
-     *            Raw row slice as returned from Hector API, of the type
-     *            <code>ColumnSlice<String, byte[]></code>
-     * @return
-     */
-    public <T, I> T load(Class<T> clazz, I id, ColumnSlice<String, byte[]> colSlice) {
-        if (null == clazz) {
-            throw new IllegalArgumentException("clazz cannot be null");
-        }
-        if (null == id) {
-            throw new IllegalArgumentException("id cannot be null");
-        }
+    T obj = objMapper.createObject(cfMapDef, id, colSlice);
+    return obj;
+  }
 
-        CFMappingDef<T, I> cfMapDef = cacheMgr.getCfMapDef(clazz, false);
-        if (null == cfMapDef) {
-            throw new IllegalStateException("No class annotated with @Entity for type, " + clazz.getName());
-        }
-
-        T obj = objMapper.createObject(cfMapDef, id, colSlice);
-        return obj;
+  /**
+   * Save the entity instance.
+   * 
+   * @param <T>
+   * @param obj
+   * @return
+   */
+  public <T> T save(T obj) {
+    if (null == obj) {
+      throw new IllegalArgumentException("object to save cannot be null");
     }
-
-    /**
-     * Save the entity instance.
-     * 
-     * @param <T>
-     * @param obj
-     * @return
-     */
-    public <T> T save(T obj) {
-        if (null == obj) {
-            throw new IllegalArgumentException("object to save cannot be null");
-        }
-        return objMapper.saveObj(keyspace, obj);
-    }
+    return objMapper.saveObj(keyspace, obj);
+  }
 }
--- object-mapper/src/main/java/me/prettyprint/hom/PropertyMappingDefinition.java
+++ object-mapper/src/main/java/me/prettyprint/hom/PropertyMappingDefinition.java
@@ -6,33 +6,33 @@ import me.prettyprint.hom.converters.Converter;
 
 
 public class PropertyMappingDefinition<T> {
-    private PropertyDescriptor propDesc;
-    private String colName;
-    private Converter<T> converter;
+  private PropertyDescriptor propDesc;
+  private String colName;
+  private Converter<T> converter;
 
-    public PropertyMappingDefinition(PropertyDescriptor propDesc, String colName, Class<Converter<T>> converter)
-            throws InstantiationException, IllegalAccessException {
-        this.propDesc = propDesc;
-        this.colName = colName;
-        this.converter = (Converter<T>) converter.newInstance();
-    }
+  public PropertyMappingDefinition(PropertyDescriptor propDesc, String colName, Class<Converter<T>> converter)
+  throws InstantiationException, IllegalAccessException {
+    this.propDesc = propDesc;
+    this.colName = colName;
+    this.converter = (Converter<T>) converter.newInstance();
+  }
 
-    @Override
-    public String toString() {
-        return "PropertyMappingDefinition [colName=" + colName + ", converter=" + converter + ", propDesc=" + propDesc
-                + "]";
-    }
+  @Override
+  public String toString() {
+    return "PropertyMappingDefinition [colName=" + colName + ", converter=" + converter + ", propDesc=" + propDesc
+    + "]";
+  }
 
-    public PropertyDescriptor getPropDesc() {
-        return propDesc;
-    }
+  public PropertyDescriptor getPropDesc() {
+    return propDesc;
+  }
 
-    public String getColName() {
-        return colName;
-    }
+  public String getColName() {
+    return colName;
+  }
 
-    public Converter<T> getConverter() {
-        return converter;
-    }
+  public Converter<T> getConverter() {
+    return converter;
+  }
 }
 
