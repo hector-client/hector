ce2ca6e601121524e3d9c33088907ae89c7fe789
zznate <nate@vervewireless.com>
2010-12-30 11:01:48 -0600

    convert emtest methods over to jpa implementations

--- object-mapper/src/test/java/me/prettyprint/hom/EntityManagerTest.java
+++ object-mapper/src/test/java/me/prettyprint/hom/EntityManagerTest.java
@@ -33,8 +33,8 @@ public class EntityManagerTest extends CassandraTestBase {
     o1.setIntProp1(1);
     o1.setBoolProp1(Boolean.TRUE);
     o1.setLongProp1(123L);
-    em.save(o1);
-    MyTestBean o2 = em.load(MyTestBean.class, o1.getBaseId());
+    em.persist(o1);
+    MyTestBean o2 = em.find(MyTestBean.class, o1.getBaseId());
 
     assertEquals(o1.getBaseId(), o2.getBaseId());
     assertEquals(o1.getIntProp1(), o2.getIntProp1());
@@ -45,7 +45,6 @@ public class EntityManagerTest extends CassandraTestBase {
   @Test
   @Ignore
   public void testInitializeSaveLoadCustomId() {
-    // TODO fixme - custom ID support
     EntityManagerImpl em = new EntityManagerImpl(keyspace, "me.prettyprint.hom.beans");
     MyCustomIdBean o1 = new MyCustomIdBean();
     o1.setId(Colors.GREEN);
