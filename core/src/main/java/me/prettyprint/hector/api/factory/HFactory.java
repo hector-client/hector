8745ab810008022cd4251bf1d69b69c472fa5e79
Peter Lin <woolfel@gmail.com>
2011-01-15 08:48:04 -0500

    adding javadocs to cluster methods. slowly adding more javadoc now that I have git installed.

--- core/src/main/java/me/prettyprint/hector/api/factory/HFactory.java
+++ core/src/main/java/me/prettyprint/hector/api/factory/HFactory.java
@@ -80,8 +80,24 @@ public final class HFactory {
   public static Cluster getCluster(String clusterName) {
     return clusters.get(clusterName);
   }
+  
   /**
+   * Method tries to create a Cluster instance for an
+   * existing Cassandra cluster. If another class already
+   * called getOrCreateCluster, the factory returns the
+   * cached instance. If the instance doesn't exist in
+   * memory, a new ThriftCluster is created and cached.
    *
+   * Example usage for a default installation of Cassandra.
+   *
+   * String clusterName = "Test Cluster";
+   * String host = "localhost:9160";
+   * Cluster cluster = HFactory.getOrCreateCluster(clusterName, host);
+   *
+   * Note the host should be the hostname and port number.
+   * It is preferable to use the hostname instead of the IP
+   * address.
+   * 
    * @param clusterName The cluster name. This is an identifying string for the cluster, e.g.
    * "production" or "test" etc. Clusters will be created on demand per each unique clusterName key.
    * @param hostIp host:ip format string
@@ -91,6 +107,23 @@ public final class HFactory {
     return getOrCreateCluster(clusterName, new CassandraHostConfigurator(hostIp));
   }
 
+  /**
+   * Method tries to create a Cluster instance for an
+   * existing Cassandra cluster. If another class already
+   * called getOrCreateCluster, the factory returns the
+   * cached instance. If the instance doesn't exist in
+   * memory, a new ThriftCluster is created and cached.
+   *
+   * Example usage for a default installation of Cassandra.
+   *
+   * String clusterName = "Test Cluster";
+   * String host = "localhost:9160";
+   * Cluster cluster = HFactory.getOrCreateCluster(clusterName, new CassandraHostConfigurator(host));
+   *
+   * @param clusterName The cluster name. This is an identifying string for the cluster, e.g.
+   * "production" or "test" etc. Clusters will be created on demand per each unique clusterName key.
+   * @param cassandraHostConfigurator
+   */
   public static Cluster getOrCreateCluster(String clusterName,
       CassandraHostConfigurator cassandraHostConfigurator) {
     synchronized (clusters) {
@@ -103,10 +136,28 @@ public final class HFactory {
     }
   }
 
+  /**
+   * Method looks in the cache for the cluster by name. If
+   * none exists, a new ThriftCluster instance is created.
+   *
+   * @param clusterName The cluster name. This is an identifying string for the cluster, e.g.
+   * "production" or "test" etc. Clusters will be created on demand per each unique clusterName key.
+   * @param cassandraHostConfigurator
+   * 
+   */
   public static Cluster createCluster(String clusterName, CassandraHostConfigurator cassandraHostConfigurator) {    
     return clusters.get(clusterName) == null ? new ThriftCluster(clusterName, cassandraHostConfigurator) : clusters.get(clusterName);
   }
 
+  /**
+   * Method looks in the cache for the cluster by name. If
+   * none exists, a new ThriftCluster instance is created.
+   *
+   * @param clusterName The cluster name. This is an identifying string for the cluster, e.g.
+   * "production" or "test" etc. Clusters will be created on demand per each unique clusterName key.
+   * @param cassandraHostConfigurator
+   * @param credentials
+   */
   public static Cluster createCluster(String clusterName, CassandraHostConfigurator cassandraHostConfigurator, Map<String, String> credentials) {
     return clusters.get(clusterName) == null ? new ThriftCluster(clusterName, cassandraHostConfigurator, credentials) : clusters.get(clusterName);    
   }
