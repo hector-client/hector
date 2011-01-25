98ebfdc0146aa9a8365f18ac2a7dc4eac042926b
zznate <nate@vervewireless.com>
2011-01-24 20:50:00 -0600

    added debug logging and javadoc comments

--- object-mapper/src/main/java/me/prettyprint/hom/service/EntitySchemaManager.java
+++ object-mapper/src/main/java/me/prettyprint/hom/service/EntitySchemaManager.java
@@ -12,6 +12,11 @@ import me.prettyprint.hector.api.exceptions.HInvalidRequestException;
 import me.prettyprint.hom.openjpa.EntityFacade;
 import me.prettyprint.hom.service.EntitySchemaStatus.SchemaResult;
 
+/**
+ * A service to manage the schema of JPA Entities
+ * 
+ * @author zznate
+ */
 public class EntitySchemaManager {
   
   private static Logger log = LoggerFactory.getLogger(EntitySchemaManager.class);
@@ -24,17 +29,30 @@ public class EntitySchemaManager {
     this.keyspace = keyspace;
   }
 
+  /**
+   * Creates the schema if it does not already exist. If it does exist,
+   * we catch the HInvalidRequestException and return an {@link EntitySchemaStatus}
+   * with {@link SchemaResult#NOT_MODIFIED}
+   * @param entityFacade
+   * @return {@link EntitySchemaStatus} with {@link SchemaResult#CREATED} if successful
+   */
   public EntitySchemaStatus createSchema(EntityFacade entityFacade) {
+    if ( log.isDebugEnabled() ) {
+      log.debug("EntitySchemaManager creating schema for: {}", entityFacade);
+    }
     BasicColumnFamilyDefinition columnFamilyDefinition = new BasicColumnFamilyDefinition();
     columnFamilyDefinition.setKeyspaceName(keyspace.getKeyspaceName());
     columnFamilyDefinition.setName(entityFacade.getColumnFamilyName());    
-    
+    // TODO determine how to pull in other CF creation settings... custom annotations?
     ColumnFamilyDefinition cfDef = new ThriftCfDef(columnFamilyDefinition);
     EntitySchemaStatus entitySchemaStatus;
     try {
       cluster.addColumnFamily(cfDef);    
       entitySchemaStatus = new EntitySchemaStatus(SchemaResult.CREATED);
     } catch (HInvalidRequestException ire) {
+      if ( log.isDebugEnabled() ) {
+        log.debug("Caught HInvalidRequestException on createSchema. Details {}", ire.getMessage());
+      }
       entitySchemaStatus = new EntitySchemaStatus(SchemaResult.NOT_MODIFIED); 
     }
     return entitySchemaStatus;
