7ef8dc231b11d2ad59a68aea5655e7ec7c7b2eb2
Shu <shu@shu-laptop.(none)>
2011-02-11 13:06:39 -0800

    added serializers for JAXB serialization

---
+++ core/src/test/java/me/prettyprint/cassandra/serializers/FastInfosetSerializerTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2011 Medio All rights reserved.
+ * 
+ */
+package me.prettyprint.cassandra.serializers;
+
+import me.prettyprint.hector.api.Serializer;
+
+/**
+ * @author shuzhang0@gmail.com
+ *
+ */
+public class FastInfosetSerializerTest extends JaxbSerializerTest {
+
+    @Override
+    protected Serializer<Object> getSerializer() {
+        return new FastInfosetSerializer(JaxbString.class);
+    }
+
+}
---
+++ core/src/test/java/me/prettyprint/cassandra/serializers/JaxbSerializerTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011 Medio All rights reserved.
+ * 
+ */
+package me.prettyprint.cassandra.serializers;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import me.prettyprint.hector.api.Serializer;
+
+/**
+ * Unit test for {@link JaxbSerializer}.
+ * @author shuzhang0@gmail.com
+ *
+ */
+public class JaxbSerializerTest extends SerializerBaseTest<Object> {
+
+    @Override
+    protected Serializer<Object> getSerializer() {
+        return new JaxbSerializer(JaxbString.class);
+    }
+
+    @Override
+    protected Collection<Object> getTestData() {
+        return Collections.singleton((Object)new JaxbString("test"));
+    }
+
+    /** A simple wrapper for a string to make it JAXB compatible. */
+    @XmlRootElement()
+    @XmlAccessorType(XmlAccessType.FIELD)
+    static class JaxbString {
+        private String m_str;
+        JaxbString(String str) {
+            m_str = str;
+        }
+
+        /** private constructor for JAXB compatibility. */
+        private JaxbString() {
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof JaxbString)) {
+                return false;
+            }
+
+            if (m_str == null) {
+                return ((JaxbString)obj).m_str == null;
+            }
+
+            return (m_str.equals(((JaxbString)obj).m_str));
+        }
+    }
+}
