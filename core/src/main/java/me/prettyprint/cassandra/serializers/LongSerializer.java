46d661fc4dd3babea75b01f1c35d8e2f3a32009b
zznate <nate@vervewireless.com>
2011-02-12 10:09:09 -0600

    cleaner use of BB for longSerializer

--- core/src/main/java/me/prettyprint/cassandra/serializers/LongSerializer.java
+++ core/src/main/java/me/prettyprint/cassandra/serializers/LongSerializer.java
@@ -21,10 +21,7 @@ public final class LongSerializer extends AbstractSerializer<Long> {
     if (obj == null) {
       return null;
     }
-    ByteBuffer b = ByteBuffer.allocate(8);
-    b.putLong(obj);
-    b.rewind();
-    return b;
+    return ByteBuffer.allocate(8).putLong(0, obj);    
   }
 
   @Override
