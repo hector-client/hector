822485c753afe7ce26a6314dd804b42c7aa09186
zznate <nate@vervewireless.com>
2011-02-21 14:28:32 -0600

    rethrow on HUnavailableException since we cannot satisfy the requested CL and never will be able to (hence futility of retrying)

--- core/src/main/java/me/prettyprint/hector/api/exceptions/HUnavailableException.java
+++ core/src/main/java/me/prettyprint/hector/api/exceptions/HUnavailableException.java
@@ -6,13 +6,15 @@ package me.prettyprint.hector.api.exceptions;
  */
 public final class HUnavailableException extends HectorException {
 
+  private static final String ERR_MSG = 
+    ": May not be enough replicas present to handle consistency level.";
   private static final long serialVersionUID = 1971498442136497970L;
 
   public HUnavailableException(String s) {
-    super(s);
+    super(s + ERR_MSG);
   }
 
   public HUnavailableException(Throwable t) {
-    super(t);
+    super(ERR_MSG,t);
   }
 }
