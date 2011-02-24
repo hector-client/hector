e16a69112529e4e83ff64d8b7ea5276f274d4b95
zznate <nate@vervewireless.com>
2011-02-24 01:30:56 -0600

    minor cleanup of thrift BB usage, added bytebuffer deSer and rewind to test case as an example

--- core/src/test/java/me/prettyprint/cassandra/model/ColumnSliceTest.java
+++ core/src/test/java/me/prettyprint/cassandra/model/ColumnSliceTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.nio.ByteBuffer;
 
 import junit.framework.Assert;
+import me.prettyprint.cassandra.serializers.ByteBufferSerializer;
 import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
 import me.prettyprint.hector.api.beans.ColumnSlice;
@@ -19,12 +20,12 @@ import org.junit.Test;
  *
  */
 public class ColumnSliceTest {
-
+  StringSerializer se = StringSerializer.get();
+  LongSerializer le = LongSerializer.get();
+  
 
   @Test
   public void testConstruction() {
-    StringSerializer se = StringSerializer.get();
-    LongSerializer le = LongSerializer.get();
     List<Column> tColumns = new ArrayList<Column>();
     ColumnSlice<String, Long> slice = new ColumnSliceImpl<String, Long>(tColumns, se, le);
     Assert.assertTrue(slice.getColumns().isEmpty());
@@ -37,5 +38,19 @@ public class ColumnSliceTest {
     tColumns.add(new Column(se.toByteBuffer("1"), le.toByteBuffer(1L), 0L));
     slice = new ColumnSliceImpl<String, Long>(tColumns, se, le);
     Assert.assertEquals((Long) 1L, slice.getColumnByName("1").getValue());
+  }
+  
+  @Test
+  public void testMultiCallOnByteBuffer() {
+    List<Column> tColumns = new ArrayList<Column>();
+    tColumns.add(new Column(se.toByteBuffer("1"), ByteBuffer.wrap("colvalue".getBytes()), 0L));
+    ColumnSlice<String, ByteBuffer> slice = new ColumnSliceImpl<String, ByteBuffer>(tColumns, se, ByteBufferSerializer.get());
+        
+    ByteBuffer value = slice.getColumnByName("1").getValue();
+    Assert.assertEquals("colvalue", se.fromByteBuffer(value));
+    value.rewind();
+    Assert.assertEquals("colvalue", se.fromByteBuffer(value));
+    value.rewind();
+    Assert.assertEquals("colvalue", se.fromByteBuffer(value));
   }
 }
