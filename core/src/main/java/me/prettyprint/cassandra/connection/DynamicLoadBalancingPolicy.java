b938e38e54bb0ccb410d226925f50a203a1333a2
Vijay Parthasarathy <vparthasarathy@lgml-vparthasarathy.netflix.com>
2011-03-01 15:47:29 -0800

    update format.
     Committer: Vijay Parthasarathy <vijay2win@gmail.com>

--- core/src/main/java/me/prettyprint/cassandra/connection/DynamicLoadBalancingPolicy.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/DynamicLoadBalancingPolicy.java
@@ -28,174 +28,174 @@ import com.google.common.collect.Maps;
 public class DynamicLoadBalancingPolicy implements LoadBalancingPolicy
 {
 
-    private static final long serialVersionUID = -1044985880174118325L;
-    private static final Logger log = LoggerFactory.getLogger(DynamicLoadBalancingPolicy.class);
-    public static RetryingScheduledThreadPoolExecutor tasks = new RetryingScheduledThreadPoolExecutor("BGTasks");
+  private static final long serialVersionUID = -1044985880174118325L;
+  private static final Logger log = LoggerFactory.getLogger(DynamicLoadBalancingPolicy.class);
+  public static RetryingScheduledThreadPoolExecutor tasks = new RetryingScheduledThreadPoolExecutor("BGTasks");
 
-    // references which is used to make the real time requests faster.
-    private Map<HClientPool, Double> scores = Maps.newConcurrentMap();
-    private List<LatencyAwareHClientPool> allPools = new CopyOnWriteArrayList<LatencyAwareHClientPool>();
+  // references which is used to make the real time requests faster.
+  private Map<HClientPool, Double> scores = Maps.newConcurrentMap();
+  private List<LatencyAwareHClientPool> allPools = new CopyOnWriteArrayList<LatencyAwareHClientPool>();
 
-    // default values this can be changed by the Client.
-    private int UPDATE_INTERVAL = 100;
-    private int RESET_INTERVAL = 20000;
-    private double DYNAMIC_BADNESS_THRESHOLD = 0.10;
+  // default values this can be changed by the Client.
+  private int UPDATE_INTERVAL = 100;
+  private int RESET_INTERVAL = 20000;
+  private double DYNAMIC_BADNESS_THRESHOLD = 0.10;
 
-    public DynamicLoadBalancingPolicy()
+  public DynamicLoadBalancingPolicy()
+  {
+
+    // Pre-calculate the scores so as we can compare it fast.
+    Runnable updateThread = new Runnable()
     {
+      public void run()
+      {
+        updateScores();
+      }
+    };
 
-        // Pre-calculate the scores so as we can compare it fast.
-        Runnable updateThread = new Runnable()
-        {
-            public void run()
-            {
-                updateScores();
-            }
-        };
-
-        // Clear Stats.
-        Runnable resetThread = new Runnable()
-        {
-            public void run()
-            {
-                for (LatencyAwareHClientPool pool : allPools)
-                {
-                    pool.clear();
-                }
-            }
-        };
-        tasks.scheduleWithFixedDelay(updateThread, UPDATE_INTERVAL, UPDATE_INTERVAL, TimeUnit.MILLISECONDS);
-        tasks.scheduleWithFixedDelay(resetThread, RESET_INTERVAL, RESET_INTERVAL, TimeUnit.MILLISECONDS);
-    }
-
-    @Override
-    public HClientPool getPool(Collection<HClientPool> pools, Set<CassandraHost> excludeHosts)
+    // Clear Stats.
+    Runnable resetThread = new Runnable()
     {
-        List<HClientPool> poolList = Lists.newArrayList(pools);
-
-        // remove the hosts from the list.
-        if (excludeHosts != null)
-        {
-            filter(poolList, excludeHosts);
-        }
-
-        Collections.shuffle(poolList);
-        HClientPool fp = poolList.get(0);
-        Double first = scores.get(fp);
-        for (int i = 1; i < poolList.size(); i++)
-        {
-            HClientPool np = poolList.get(i);
-            Double next = scores.get(np);
-            if ((first - next) / first > DYNAMIC_BADNESS_THRESHOLD)
-            {
-                Collections.sort(poolList, new SortByScoreComparator());
-                if (log.isDebugEnabled())
-                    log.debug(String.format("According to score we have chosen {} vs first {}", poolList.get(0), fp));
-                break;
-            }
-        }
-        return poolList.get(0);
-    }
-
-    private void filter(List<HClientPool> from, Set<CassandraHost> subList)
-    {
-        Iterator<HClientPool> it = from.iterator();
-        while (it.hasNext())
-        {
-            if (subList.contains(it.next().getCassandraHost()))
-                it.remove();
-        }
-    }
-
-    private class SortByScoreComparator implements Comparator<HClientPool>
-    {
-        public int compare(HClientPool p1, HClientPool p2)
-        {
-            Double scored1 = scores.get(p1);
-            Double scored2 = scores.get(p2);
-            if (scored1.equals(scored2))
-                return 0;
-            if (scored1 < scored2)
-                return -1;
-            else
-                return 1;
-        }
-    }
-
-    @Override
-    public HClientPool createConnection(CassandraHost host)
-    {
-        LatencyAwareHClientPool pool = new LatencyAwareHClientPool(host);
-        add(pool);
-        return pool;
-    }
-
-    // This is helper class for the test cases. TODO: cleanup.
-    void add(LatencyAwareHClientPool pool)
-    {
-        allPools.add(pool);
-        // update the reference of the scores intially it is Zero.
-        scores.put(pool, 0.0);
-    }
-
-    // This will be a expensive call.
-    void updateScores()
-    {
+      public void run()
+      {
         for (LatencyAwareHClientPool pool : allPools)
         {
-            scores.put(pool, pool.score());
-            pool.resetIntervel();
+          pool.clear();
         }
+      }
+    };
+    tasks.scheduleWithFixedDelay(updateThread, UPDATE_INTERVAL, UPDATE_INTERVAL, TimeUnit.MILLISECONDS);
+    tasks.scheduleWithFixedDelay(resetThread, RESET_INTERVAL, RESET_INTERVAL, TimeUnit.MILLISECONDS);
+  }
+
+  @Override
+  public HClientPool getPool(Collection<HClientPool> pools, Set<CassandraHost> excludeHosts)
+  {
+    List<HClientPool> poolList = Lists.newArrayList(pools);
+
+    // remove the hosts from the list.
+    if (excludeHosts != null)
+    {
+      filter(poolList, excludeHosts);
     }
 
-    public int getUpdateInterval()
+    Collections.shuffle(poolList);
+    HClientPool fp = poolList.get(0);
+    Double first = scores.get(fp);
+    for (int i = 1; i < poolList.size(); i++)
     {
-        return UPDATE_INTERVAL;
+      HClientPool np = poolList.get(i);
+      Double next = scores.get(np);
+      if ((first - next) / first > DYNAMIC_BADNESS_THRESHOLD)
+      {
+        Collections.sort(poolList, new SortByScoreComparator());
+        if (log.isDebugEnabled())
+          log.debug(String.format("According to score we have chosen {} vs first {}", poolList.get(0), fp));
+        break;
+      }
     }
+    return poolList.get(0);
+  }
 
-    /**
-     * Set the configured interval for the stats to be recalculated (until this time it is been cached.
-     * 
-     * @param updateInterval
-     *            In ms.
-     */
-    public void setUpdateInterval(int updateInterval)
+  private void filter(List<HClientPool> from, Set<CassandraHost> subList)
+  {
+    Iterator<HClientPool> it = from.iterator();
+    while (it.hasNext())
     {
-        UPDATE_INTERVAL = updateInterval;
+      if (subList.contains(it.next().getCassandraHost()))
+        it.remove();
     }
+  }
 
-    public int getResetInterval()
+  private class SortByScoreComparator implements Comparator<HClientPool>
+  {
+    public int compare(HClientPool p1, HClientPool p2)
     {
-        return RESET_INTERVAL;
+      Double scored1 = scores.get(p1);
+      Double scored2 = scores.get(p2);
+      if (scored1.equals(scored2))
+        return 0;
+      if (scored1 < scored2)
+        return -1;
+      else
+        return 1;
     }
+  }
 
-    /**
-     * Set the configured interval for the stats to be reset so that the new stats are allowed and we can get rid of bad
-     * nodes value. This is under the assumption that the bad nodes will eventually get better....
-     * 
-     * @param resetInterval
-     *            in ms
-     */
-    public void setResetInterval(int resetInterval)
-    {
-        RESET_INTERVAL = resetInterval;
-    }
+  @Override
+  public HClientPool createConnection(CassandraHost host)
+  {
+    LatencyAwareHClientPool pool = new LatencyAwareHClientPool(host);
+    add(pool);
+    return pool;
+  }
 
-    public double getBadnessThreshold()
-    {
-        return DYNAMIC_BADNESS_THRESHOLD;
-    }
+  // This is helper class for the test cases. TODO: cleanup.
+  void add(LatencyAwareHClientPool pool)
+  {
+    allPools.add(pool);
+    // update the reference of the scores intially it is Zero.
+    scores.put(pool, 0.0);
+  }
 
-    /**
-     * This is the percentage of badness which is acceptable... 
-     * 
-     * Example: A should be 0.20 (20%) bad than B before B is choosen rathar than A.
-     * 
-     * @param badness
-     *            in %
-     */
-    public void setBadnessThreshold(double badness)
+  // This will be a expensive call.
+  void updateScores()
+  {
+    for (LatencyAwareHClientPool pool : allPools)
     {
-        DYNAMIC_BADNESS_THRESHOLD = badness;
+      scores.put(pool, pool.score());
+      pool.resetIntervel();
     }
+  }
+
+  public int getUpdateInterval()
+  {
+    return UPDATE_INTERVAL;
+  }
+
+  /**
+   * Set the configured interval for the stats to be recalculated (until this time it is been cached.
+   * 
+   * @param updateInterval
+   *          In ms.
+   */
+  public void setUpdateInterval(int updateInterval)
+  {
+    UPDATE_INTERVAL = updateInterval;
+  }
+
+  public int getResetInterval()
+  {
+    return RESET_INTERVAL;
+  }
+
+  /**
+   * Set the configured interval for the stats to be reset so that the new stats are allowed and we can get rid of bad
+   * nodes value. This is under the assumption that the bad nodes will eventually get better....
+   * 
+   * @param resetInterval
+   *          in ms
+   */
+  public void setResetInterval(int resetInterval)
+  {
+    RESET_INTERVAL = resetInterval;
+  }
+
+  public double getBadnessThreshold()
+  {
+    return DYNAMIC_BADNESS_THRESHOLD;
+  }
+
+  /**
+   * This is the percentage of badness which is acceptable...
+   * 
+   * Example: A should be 0.20 (20%) bad than B before B is choosen rathar than A.
+   * 
+   * @param badness
+   *          in %
+   */
+  public void setBadnessThreshold(double badness)
+  {
+    DYNAMIC_BADNESS_THRESHOLD = badness;
+  }
 }
--- core/src/main/java/me/prettyprint/cassandra/connection/HClientPool.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/HClientPool.java
@@ -3,15 +3,14 @@ package me.prettyprint.cassandra.connection;
 import me.prettyprint.cassandra.service.CassandraHost;
 import me.prettyprint.hector.api.exceptions.HectorException;
 
-public interface HClientPool extends PoolMetric {
-
-	public HThriftClient borrowClient() throws HectorException;
-	public CassandraHost getCassandraHost();
-	public int getNumBeforeExhausted();
-	public boolean isExhausted();
-	public int getMaxActive();
-	public String getStatusAsString();
-	public void releaseClient(HThriftClient client)
-			throws HectorException;
-	void shutdown();
+public interface HClientPool extends PoolMetric
+{
+  public HThriftClient borrowClient() throws HectorException;
+  public CassandraHost getCassandraHost();
+  public int getNumBeforeExhausted();
+  public boolean isExhausted();
+  public int getMaxActive();
+  public String getStatusAsString();
+  public void releaseClient(HThriftClient client) throws HectorException;
+  void shutdown();
 }
