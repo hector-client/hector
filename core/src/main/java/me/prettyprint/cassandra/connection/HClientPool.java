2be9f6ff7e759ea478f2c3536bb92ece7f8a26c2
Vijay Parthasarathy <vparthasarathy@lgml-vparthasarathy.netflix.com>
2011-03-02 09:39:42 -0800

    fixing spacing and new line format. Thanks!
    Committer: Vijay Parthasarathy <vijay2win@gmai.com>

--- core/src/main/java/me/prettyprint/cassandra/connection/HClientPool.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/HClientPool.java
@@ -3,8 +3,7 @@ package me.prettyprint.cassandra.connection;
 import me.prettyprint.cassandra.service.CassandraHost;
 import me.prettyprint.hector.api.exceptions.HectorException;
 
-public interface HClientPool extends PoolMetric
-{
+public interface HClientPool extends PoolMetric {
   public HThriftClient borrowClient() throws HectorException;
   public CassandraHost getCassandraHost();
   public int getNumBeforeExhausted();
