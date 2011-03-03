6b96281695c9697d4bf36ee3ba62209fd5e716e5
zznate <nate@vervewireless.com>
2011-03-02 21:06:59 -0600

    stuck charset in StringSerializer to UTF8. Thanks to alexism for the patch

--- core/src/main/java/me/prettyprint/cassandra/serializers/StringSerializer.java
+++ core/src/main/java/me/prettyprint/cassandra/serializers/StringSerializer.java
@@ -15,7 +15,7 @@ public final class StringSerializer extends AbstractSerializer<String> {
 
   private static final String UTF_8 = "UTF-8";
   private static final StringSerializer instance = new StringSerializer();
-  private static final Charset charset = Charset.defaultCharset();
+  private static final Charset charset = Charset.forName(UTF_8);
 
   public static StringSerializer get() {
     return instance;
@@ -25,12 +25,8 @@ public final class StringSerializer extends AbstractSerializer<String> {
   public ByteBuffer toByteBuffer(String obj) {
     if (obj == null) {
       return null;
-    }
-    try {
-      return ByteBuffer.wrap(obj.getBytes(charset.name()));
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException(e);
-    }
+    }    
+    return ByteBuffer.wrap(obj.getBytes(charset));    
   }
 
   @Override
