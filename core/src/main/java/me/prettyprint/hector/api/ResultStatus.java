aacd1f922dc0b63383d72a52b891909100c98c9d
zznate <nate@vervewireless.com>
2011-03-04 16:49:54 -0600

    pulled up top level interface for executiontime and hostused

---
+++ core/src/main/java/me/prettyprint/hector/api/ResultStatus.java
@@ -0,0 +1,27 @@
+package me.prettyprint.hector.api;
+
+import me.prettyprint.cassandra.service.CassandraHost;
+
+/**
+ * Common interface for tracking the {@link CassandraHost} used
+ * for the execution of the operation and how long that operation
+ * took in micro seconds
+ *
+ * @author zznate
+ */
+public interface ResultStatus {
+
+  /**
+   * How long the operation took to execute in MICRO-seconds. Internally
+   * this is usually the difference between two calls of {@link System#nanoTime()} 
+   * divided by 1000
+   * @return
+   */
+  long getExecutionTimeMicro();
+
+  /**
+   * The {@link CassandraHost} on which this operation
+   * was successful
+   */
+  CassandraHost getHostUsed();
+}
