499c338d63622db665191309d5fad1b7446cd963
zznate <nate@vervewireless.com>
2011-03-10 13:29:41 -0600

    changed back to collection on pool impls keeping steady state semantics of NBHM values for now

--- core/src/main/java/me/prettyprint/cassandra/connection/LoadBalancingPolicy.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/LoadBalancingPolicy.java
@@ -1,12 +1,13 @@
 package me.prettyprint.cassandra.connection;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 import me.prettyprint.cassandra.service.CassandraHost;
 
 public interface LoadBalancingPolicy extends Serializable {
-  HClientPool getPool(List<HClientPool> pools, Set<CassandraHost> excludeHosts);
+  HClientPool getPool(Collection<HClientPool> pools, Set<CassandraHost> excludeHosts);
   HClientPool createConnection(CassandraHost host);
 }
