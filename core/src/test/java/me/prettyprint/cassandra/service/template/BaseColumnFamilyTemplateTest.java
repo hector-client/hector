8c303dd5649687502b13d8082831fab4232a1a3b
zznate <nate@vervewireless.com>
2011-03-18 16:01:56 -0500

    added missing test cases

---
+++ core/src/test/java/me/prettyprint/cassandra/service/template/BaseColumnFamilyTemplateTest.java
@@ -0,0 +1,23 @@
+package me.prettyprint.cassandra.service.template;
+
+import static me.prettyprint.hector.api.factory.HFactory.createKeyspace;
+import static me.prettyprint.hector.api.factory.HFactory.getOrCreateCluster;
+
+import org.junit.Before;
+
+import me.prettyprint.cassandra.BaseEmbededServerSetupTest;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.Keyspace;
+
+public abstract class BaseColumnFamilyTemplateTest extends BaseEmbededServerSetupTest {
+
+  protected Keyspace keyspace;
+  static final StringSerializer se = StringSerializer.get();
+  
+  @Before
+  public void setupLocal() {
+    Cluster cluster = getOrCreateCluster("MyCluster", "127.0.0.1:9170");
+    keyspace = createKeyspace("Keyspace1", cluster);
+  }
+}
