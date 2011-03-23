5f2e9fb4e58ef57a577d9cd06e06d9afaad49023
Ed Anuff <ed@anuff.com>
2011-03-23 11:55:46 -0700

    rename DynamicCompositeTest to CompositeTest

--- core/src/test/java/me/prettyprint/hector/api/CompositeTest.java
+++ core/src/test/java/me/prettyprint/hector/api/CompositeTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DynamicCompositeTest {
+public class CompositeTest {
 
   private static final Logger log = LoggerFactory
       .getLogger(ClockResolutionTest.class);
