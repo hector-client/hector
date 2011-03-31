eb5bdef40c382d92d708f80e30e3a9b33a5a8037
zznate <nate@vervewireless.com>
2011-03-31 18:19:16 -0500

    supercol inserts working on template innards

--- core/src/test/java/me/prettyprint/cassandra/service/template/SuperCfTemplateTest.java
+++ core/src/test/java/me/prettyprint/cassandra/service/template/SuperCfTemplateTest.java
@@ -2,8 +2,6 @@ package me.prettyprint.cassandra.service.template;
 
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -14,12 +12,13 @@ public class SuperCfTemplateTest extends BaseColumnFamilyTemplateTest {
   public void testSuperCfInsertReadTemplate() {
     SuperCfTemplate<String, String, String> sTemplate = 
       new SuperCfTemplate<String, String, String>(keyspace, "Super1", se, se, se);
+    SuperCfUpdater sUpdater = sTemplate.createUpdater("skey1","super1");
+    sUpdater.setString("sub_col_1", "sub_val_1");
+    sTemplate.update(sUpdater);
+
+    SuperCfResult<String,String,String> result = sTemplate.querySuperColumn("skey1", "super1");
+    assertEquals("sub_val_1",result.getString("sub_col_1"));
   }
   
-  @Test
-  public void testSuperCfInsertRead() {
-    SuperCfTemplate<String, String, String> sTemplate = 
-      new SuperCfTemplate<String, String, String>(keyspace, "Super1", se, se, se);
-    
-  }
+  
 }
