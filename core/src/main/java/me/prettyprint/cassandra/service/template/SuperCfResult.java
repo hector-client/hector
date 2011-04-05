5592da93947ee2d18e47059a07d84a81ba241738
zznate <nate@vervewireless.com>
2011-04-04 23:53:41 -0500

    super col result wrapping smoothed out, coverage for such

--- core/src/main/java/me/prettyprint/cassandra/service/template/SuperCfResult.java
+++ core/src/main/java/me/prettyprint/cassandra/service/template/SuperCfResult.java
@@ -1,5 +1,14 @@
 package me.prettyprint.cassandra.service.template;
 
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import me.prettyprint.hector.api.beans.HColumn;
+import me.prettyprint.hector.api.beans.HSuperColumn;
+
 /**
  * Holds the result for the contents of a super column. This interface add
  * access to the current super column name since this may be a property of the
@@ -14,6 +23,27 @@ package me.prettyprint.cassandra.service.template;
  *          child column name data type
  */
 public interface SuperCfResult<K, SN, N> extends ColumnFamilyResult<K, N> {
-  SN getSuperName();
+  // TODO remove. this no loger makes sense with many-to-one on a row
   
+  Collection<SN> getSuperColumns();
+  
+  K getKey();
+
+  UUID getUUID(SN sColumnName, N columnName);
+
+  String getString(SN sColumnName, N columnName);
+
+  Long getLong(SN sColumnName, N columnName);
+
+  Integer getInteger(SN sColumnName, N columnName);
+
+  Boolean getBoolean(SN sColumnName, N columnName);
+
+  byte[] getByteArray(SN sColumnName, N columnName);
+  
+  ByteBuffer getByteBuffer(SN sColumnName, N columnName);
+
+  Date getDate(SN sColumnName, N columnName);  
+  
+  void applySuperColumn(SN sColumnName);
 }
--- core/src/main/java/me/prettyprint/cassandra/service/template/SuperCfResultWrapper.java
+++ core/src/main/java/me/prettyprint/cassandra/service/template/SuperCfResultWrapper.java
@@ -1,13 +1,35 @@
 package me.prettyprint.cassandra.service.template;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import me.prettyprint.cassandra.model.ExecutionResult;
+import me.prettyprint.cassandra.model.HColumnImpl;
+import me.prettyprint.cassandra.model.HSuperColumnImpl;
+import me.prettyprint.cassandra.serializers.BooleanSerializer;
+import me.prettyprint.cassandra.serializers.ByteBufferSerializer;
+import me.prettyprint.cassandra.serializers.BytesArraySerializer;
+import me.prettyprint.cassandra.serializers.DateSerializer;
+import me.prettyprint.cassandra.serializers.IntegerSerializer;
+import me.prettyprint.cassandra.serializers.LongSerializer;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.serializers.UUIDSerializer;
 import me.prettyprint.hector.api.Serializer;
+import me.prettyprint.hector.api.beans.HColumn;
+import me.prettyprint.hector.api.beans.HSuperColumn;
 
+import org.apache.cassandra.thrift.Column;
 import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provides access to the current row of data during super column queries. 
@@ -15,31 +37,162 @@ import org.apache.cassandra.thrift.ColumnOrSuperColumn;
  * @author zznate
  * @param <N> the super column's sub column name type
  */
-public class SuperCfResultWrapper<K,SN,N> extends ColumnFamilyResultWrapper<K,N> implements SuperCfResult<K,SN,N> {
-
-
+public class SuperCfResultWrapper<K,SN,N> extends AbstractResultWrapper<K,N> implements SuperCfResult<K,SN,N> {
+  private static final Logger log = LoggerFactory.getLogger(SuperCfResultWrapper.class);
+  
+  private Map<SN,Map<N,HColumn<N,ByteBuffer>>> columns = new LinkedHashMap<SN,Map<N,HColumn<N,ByteBuffer>>>();
+  private Iterator<Map.Entry<ByteBuffer, List<ColumnOrSuperColumn>>> rows;
+  private Map.Entry<ByteBuffer, List<ColumnOrSuperColumn>> entry;
+  private ExecutionResult<Map<ByteBuffer,List<ColumnOrSuperColumn>>> executionResult;
+  private List<SN> superColumns;
+  private Map<N,HColumn<N,ByteBuffer>> subColumns = new LinkedHashMap<N,HColumn<N,ByteBuffer>>();
+  private SN currentSuperColumn;
 
   private Serializer<SN> sNameSerializer;
-  private SN sColumnName;
   
   public SuperCfResultWrapper(Serializer<K> keySerializer,
       Serializer<SN> sNameSerializer,
       Serializer<N> subSerializer,
-      ExecutionResult<Map<ByteBuffer, List<ColumnOrSuperColumn>>> executionResult,
-      SN sColumnName) {
+      ExecutionResult<Map<ByteBuffer, List<ColumnOrSuperColumn>>> executionResult) {
     super(keySerializer, subSerializer, executionResult);
     this.sNameSerializer = sNameSerializer;
-    this.sColumnName = sColumnName;
-    
+    this.rows = executionResult.get().entrySet().iterator();    
+    next();
+  }    
+
+  @Override
+  public SuperCfResult<K, SN, N> next() {
+    if ( !hasNext() ) {
+      throw new NoSuchElementException("No more rows left on this HColumnFamily");
+    }
+    entry = rows.next();    
+    log.debug("found entry{}", getKey());
+    applyToRow(entry.getValue());
+    return this;
   }
   
-
-  /**
-   * Provides access to the current super column name from the mapper objects. This may be
-   * needed when the super columm name may be part of the object being mapped into.
-   */
-  public SN getSuperName() {
-    return sColumnName; 
+  private void applyToRow(List<ColumnOrSuperColumn> cosclist) {
+    superColumns = new ArrayList<SN>(cosclist.size());
+    for (Iterator<ColumnOrSuperColumn> iterator = cosclist.iterator(); iterator.hasNext();) {
+      ColumnOrSuperColumn cosc = iterator.next();
+      SN sColName = sNameSerializer.fromByteBuffer(cosc.super_column.name);
+      log.debug("cosc {}", cosc.super_column);
+       
+      superColumns.add(sColName);
+      Iterator<Column> tcolumns = cosc.getSuper_column().getColumnsIterator();
+      Map<N,HColumn<N,ByteBuffer>> subColMap = new LinkedHashMap<N, HColumn<N,ByteBuffer>>();
+      while ( tcolumns.hasNext() ) {
+        Column col = tcolumns.next();
+        subColMap.put(columnNameSerializer.fromByteBuffer(col.name), new HColumnImpl<N, ByteBuffer>(col, columnNameSerializer, ByteBufferSerializer.get()));
+      }
+      columns.put(sColName, subColMap);
+    }
   }
+  
+  
+  public List<SN> getSuperColumns() {
+    return superColumns;
+  }
+  
+  
+  @Override
+  public ByteBuffer getColumnValue(N columnName) {
+    HColumn<N,ByteBuffer> col = getColumn( columnName );
+    return col != null ? col.getValue() : null; 
+  }
+
+
+  @Override
+  public K getKey() {
+    return keySerializer.fromByteBuffer(entry.getKey()); 
+  }
+
+
+  @Override
+  public HColumn<N, ByteBuffer> getColumn(N columnName) {
+    return subColumns == null ? null : subColumns.get( columnName );
+  }
+
+
+  @Override
+  public Collection<N> getColumnNames() {
+    return subColumns.keySet();
+  }
+
+
+  @Override
+  public boolean hasNext() {
+    return rows.hasNext();
+  }
+
+  @Override
+  public void remove() {
+    rows.remove();    
+  }
+
+  private <V> V extractType(SN sColumnName, N columnName, Serializer<V> valueSerializer) {
+    Map<N, HColumn<N, ByteBuffer>> map = columns.get(sColumnName);
+    if ( map != null && map.get(columnName) != null )
+      return valueSerializer.fromByteBuffer(map.get(columnName).getValue());
+    return null;
+  }
+  
+  @Override
+  public Boolean getBoolean(SN sColumnName, N columnName) {    
+    return extractType(sColumnName, columnName, BooleanSerializer.get());
+  }
+
+
+  @Override
+  public byte[] getByteArray(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, BytesArraySerializer.get());
+  }
+
+
+  @Override
+  public Date getDate(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, DateSerializer.get());
+  }
+
+
+  @Override
+  public Integer getInteger(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, IntegerSerializer.get());
+  }
+
+
+  @Override
+  public Long getLong(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, LongSerializer.get());
+  }
+
+
+  @Override
+  public String getString(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, StringSerializer.get());
+  }
+
+
+
+  @Override
+  public UUID getUUID(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, UUIDSerializer.get());
+  }
+
+  @Override
+  public ByteBuffer getByteBuffer(SN sColumnName, N columnName) {
+    return extractType(sColumnName, columnName, ByteBufferSerializer.get());
+  }
+
+
+
+  @Override
+  public void applySuperColumn(SN sColumnName) {
+    this.currentSuperColumn = sColumnName;
+    this.subColumns = columns.get(currentSuperColumn);
+
+  }
+  
+  
 
 }
