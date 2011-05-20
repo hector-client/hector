f0a71f28cbe4f804f442563d11d5722a5a5c5d49
zznate <nate@vervewireless.com>
2011-05-20 16:11:01 -0500

    fixed retry logic in the case of fail after one more try configuration. Also modifies some long standing (and I believe incorrect) behaviour of trying until successful on a timeout exception

--- core/src/test/java/me/prettyprint/cassandra/connection/HConnectionManagerTest.java
+++ core/src/test/java/me/prettyprint/cassandra/connection/HConnectionManagerTest.java
@@ -2,12 +2,18 @@ package me.prettyprint.cassandra.connection;
 
 import static org.junit.Assert.*;
 
+import org.apache.cassandra.thrift.Cassandra.Client;
 import org.junit.Before;
 import org.junit.Test;
 
 import me.prettyprint.cassandra.BaseEmbededServerSetupTest;
 import me.prettyprint.cassandra.service.CassandraHost;
 import me.prettyprint.cassandra.service.CassandraHostConfigurator;
+import me.prettyprint.cassandra.service.FailoverPolicy;
+import me.prettyprint.cassandra.service.Operation;
+import me.prettyprint.cassandra.service.OperationType;
+import me.prettyprint.hector.api.exceptions.HTimedOutException;
+import me.prettyprint.hector.api.exceptions.HectorException;
 import me.prettyprint.hector.api.exceptions.HectorTransportException;
 
 public class HConnectionManagerTest extends BaseEmbededServerSetupTest {
@@ -55,5 +61,36 @@ public class HConnectionManagerTest extends BaseEmbededServerSetupTest {
     assertTrue(connectionManager.suspendCassandraHost(cassandraHost));
     assertEquals(1,connectionManager.getSuspendedCassandraHosts().size());
     assertTrue(connectionManager.unsuspendCassandraHost(cassandraHost));
+  }
+  
+  @Test(expected=HTimedOutException.class)
+  public void testTimedOutOperateWithFailover() {
+    setupClient();
+    FailoverPolicy fp = FailoverPolicy.ON_FAIL_TRY_ONE_NEXT_AVAILABLE;
+    connectionManager.operateWithFailover(new TimeoutOp(fp));
+  }
+  
+  abstract class StubOp extends Operation<String> {
+
+    StubOp(FailoverPolicy fp) {      
+      this(OperationType.META_READ);
+      failoverPolicy = fp;
+    }
+    
+    public StubOp(OperationType operationType) {
+      super(operationType);
+    }    
+  }
+  
+  class TimeoutOp extends StubOp {
+    
+    TimeoutOp(FailoverPolicy fp) {
+      super(fp);
+    }
+
+    @Override
+    public String execute(Client cassandra) throws HectorException {
+      throw new HTimedOutException("fake timeout");
+    }
   }
 }
