ff12aee6e2d617e7002a11e49b0294357d07bed4
patricioe <patricioe@gmail.com>
2011-06-06 18:04:01 -0700

    add test for ClockUtils

---
+++ utils/src/test/java/me/prettyprint/cassandra/clock/ClockUtilsTest.java
@@ -0,0 +1,35 @@
+package prettyprint.cassandra.clock;
+
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import me.prettyprint.cassandra.clock.ClockUtils;
+import me.prettyprint.cassandra.clock.MicrosecondsClockResolution;
+import me.prettyprint.cassandra.clock.MicrosecondsSyncClockResolution;
+import me.prettyprint.cassandra.clock.MillisecondsClockResolution;
+import me.prettyprint.cassandra.clock.SecondsClockResolution;
+import me.prettyprint.hector.api.ClockResolution;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClockUtilsTest {
+
+	  @Test
+	  public void testCreateClockResolution() throws Exception {
+	    try {
+	      ClockUtils.createClockResolution("ItDoesNotExist");
+	      fail();
+	    } catch (RuntimeException e) {
+	        // good!
+	    }
+
+	    assertTrue(ClockUtils.createClockResolution(ClockResolution.SECONDS) instanceof SecondsClockResolution);
+	    assertTrue(ClockUtils.createClockResolution(ClockResolution.MILLISECONDS) instanceof MillisecondsClockResolution);
+	    assertTrue(ClockUtils.createClockResolution(ClockResolution.MICROSECONDS) instanceof MicrosecondsClockResolution);
+	    assertTrue(ClockUtils.createClockResolution(ClockResolution.MICROSECONDS_SYNC)
+	            instanceof MicrosecondsSyncClockResolution);
+	  }
+
+}
