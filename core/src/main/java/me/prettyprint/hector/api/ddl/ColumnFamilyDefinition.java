66e99d45b059de465555a184a019c5c9cb918668
patricioe <patricioe@gmail.com>
2011-06-20 10:58:45 -0700

    Expose setters in ColumnFamilyDefinition iface

--- core/src/main/java/me/prettyprint/hector/api/ddl/ColumnFamilyDefinition.java
+++ core/src/main/java/me/prettyprint/hector/api/ddl/ColumnFamilyDefinition.java
@@ -11,27 +11,69 @@ import java.util.List;
 public interface ColumnFamilyDefinition {
 
   String getKeyspaceName();
+  void setKeyspaceName(String keyspaceName);
+  
   String getName();
+  void setName(String name);
+  
   ColumnType getColumnType();
+  void setColumnType(ColumnType columnType);
+  
   ComparatorType getComparatorType();
+  void setComparatorType(ComparatorType comparitorType);
+  
   ComparatorType getSubComparatorType();
+  void setSubComparatorType(ComparatorType subComparitorType);
+  
   String getComment();
+  void setComment(String comment);
+  
   double getRowCacheSize();
+  void setRowCacheSize(double rowCacheSize);
+  
   int getRowCacheSavePeriodInSeconds();
+  void setRowCacheSavePeriodInSeconds(int rowCacheSavePeriodInSeconds);
+  
   int getKeyCacheSavePeriodInSeconds();
+  void setKeyCacheSavePeriodInSeconds(int keyCacheSavePeriodInSeconds);
+  
   double getKeyCacheSize();
+  void setKeyCacheSize(double keyCacheSize);
+  
   String getKeyValidationClass();
+  void setKeyValidationClass(String keyValidationClass);
+  
   double getReadRepairChance();
+  void setReadRepairChance(double readRepairChance);
+  
   List<ColumnDefinition> getColumnMetadata();
+  void addColumnDefinition( ColumnDefinition columnDefinition);
+  
   int getGcGraceSeconds();
+  void setGcGraceSeconds(int gcGraceSeconds);
+  
   String getDefaultValidationClass();
+  void setDefaultValidationClass(String defaultValidationClass);
+  
   int getId();
+  void setId(int id);
+  
   int getMaxCompactionThreshold();
+  void setMaxCompactionThreshold(int maxCompactionThreshold);
+  
   int getMinCompactionThreshold();
+  void setMinCompactionThreshold(int minCompactionThreshold);
+  
   double getMemtableOperationsInMillions();
+  void setMemtableOperationsInMillions(double memtableOperationsInMillions);
+  
   int getMemtableThroughputInMb();
+  void setMemtableThroughputInMb(int memtableThroughputInMb);
+  
   int getMemtableFlushAfterMins();
+  void setMemtableFlushAfterMins(int memtableFlushAfterMins);
+  
   boolean isReplicateOnWrite();
-
+  void setReplicateOnWrite(boolean replicateOnWrite);
 
 }
