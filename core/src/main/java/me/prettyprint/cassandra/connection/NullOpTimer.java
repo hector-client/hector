0db6060bf4f84b1a2b0263119841f7a30de24346
sean <sean@sean-System-Product-Name.(none)>
2011-07-07 22:41:12 -0700

    make op timing configurable, defaulting to no timing, but allowing for speed4j

---
+++ core/src/main/java/me/prettyprint/cassandra/connection/NullOpTimer.java
@@ -0,0 +1,13 @@
+package me.prettyprint.cassandra.connection;
+
+public class NullOpTimer implements HOpTimer {
+
+	@Override
+	public Object start() {
+		return this;
+	}
+
+	@Override
+	public void stop(Object token, String tagName,  boolean success) {}
+	
+}
