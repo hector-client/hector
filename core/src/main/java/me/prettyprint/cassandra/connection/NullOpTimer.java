94f3c214ee0d25e620e7d54a949082cb12438da5
sean <sean@sean-System-Product-Name.(none)>
2011-07-08 19:35:05 -0700

    indenting

--- core/src/main/java/me/prettyprint/cassandra/connection/NullOpTimer.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/NullOpTimer.java
@@ -2,12 +2,13 @@ package me.prettyprint.cassandra.connection;
 
 public class NullOpTimer implements HOpTimer {
 
-	@Override
-	public Object start() {
-		return this;
-	}
+  @Override
+  public Object start() {
+    return this;
+  }
 
-	@Override
-	public void stop(Object token, String tagName,  boolean success) {}
-	
+  @Override
+  public void stop(Object token, String tagName, boolean success) {
+  }
+
 }
