175e503e193e9f6574df62d4d311d5d93299953f
patricioe <patricioe@gmail.com>
2011-10-07 11:06:38 -0700

    Add new hierachy for Pool Exceptions

--- core/src/main/java/me/prettyprint/hector/api/exceptions/HPoolExhaustedException.java
+++ core/src/main/java/me/prettyprint/hector/api/exceptions/HPoolExhaustedException.java
@@ -5,9 +5,10 @@ package me.prettyprint.hector.api.exceptions;
  * Indicates that a client pool has been exhausted.
  *
  * @author Ran Tavory (ran@outbain.com)
+ * @author Patricio Echague (patricioe@gmail.com)
  *
  */
-public final class HPoolExhaustedException extends HectorException {
+public final class HPoolExhaustedException extends HPoolRecoverableException {
 
   private static final long serialVersionUID = -6200999597951673383L;
 
