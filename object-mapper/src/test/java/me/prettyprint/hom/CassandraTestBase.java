90e5483fbdd1e0982b5f6a765b2961d034ac8d32
B. Todd Burruss <btoddb@gmail.com>
2011-10-13 14:29:11 -0700

    clean up pom and use hector/pom.xml definitions.  update to work with hector 1.0 and cassandra 1.0.0

--- object-mapper/src/test/java/me/prettyprint/hom/CassandraTestBase.java
+++ object-mapper/src/test/java/me/prettyprint/hom/CassandraTestBase.java
@@ -100,7 +100,7 @@ public class CassandraTestBase {
             .setKey_cache_size(0).setRow_cache_size(0).setGc_grace_seconds(86400));
         cfDefList.add(new CfDef("TestKeyspace", "Furniture").setComparator_type(BytesType.class.getSimpleName())
             .setKey_cache_size(0).setRow_cache_size(0).setGc_grace_seconds(86400));
-        cluster = HFactory.getOrCreateCluster("TestPool", "localhost:9161");
+        cluster = HFactory.getOrCreateCluster("TestPool", "localhost:9170");
         createKeyspace(cluster, "TestKeyspace", "org.apache.cassandra.locator.SimpleStrategy", 1, cfDefList);
         keyspace = HFactory.createKeyspace("TestKeyspace", cluster);
       }
