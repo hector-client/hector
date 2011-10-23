50ef9cf1c9fd4cfe5891535736a354a6904c84b4
Jim Ancona <jim@anconafamily.com>
2011-10-23 09:42:07 -0400

    Add support for Thrift get_indexed_slices to ColumnFamilyTemplate

--- core/src/main/java/me/prettyprint/cassandra/service/template/ThriftColumnFamilyTemplate.java
+++ core/src/main/java/me/prettyprint/cassandra/service/template/ThriftColumnFamilyTemplate.java
@@ -15,10 +15,11 @@ import me.prettyprint.cassandra.service.OperationType;
 import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.Serializer;
 import me.prettyprint.hector.api.exceptions.HectorException;
-import me.prettyprint.hector.api.mutation.Mutator;
 
 import org.apache.cassandra.thrift.Cassandra;
+import org.apache.cassandra.thrift.Cassandra.Client;
 import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.apache.cassandra.thrift.KeySlice;
 
 import com.google.common.collect.Iterators;
 
@@ -36,16 +37,16 @@ public class ThriftColumnFamilyTemplate<K, N> extends ColumnFamilyTemplate<K, N>
       Serializer<K> keySerializer, Serializer<N> topSerializer) {
     super(keyspace, columnFamily, keySerializer, topSerializer);
   }
-  
+
   public <T> T doExecuteSlice(K key, HSlicePredicate<N> predicate, ColumnFamilyRowMapper<K, N, T> mapper) {
     return mapper.mapRow(doExecuteSlice(key,predicate));
   }
-    
+
   public ColumnFamilyResult<K, N> doExecuteSlice(final K key, final HSlicePredicate<N> workingSlicePredicate) {    
     return new ColumnFamilyResultWrapper<K, N>(keySerializer, topSerializer, 
         sliceInternal(key, workingSlicePredicate));
   }
-    
+
   public ColumnFamilyResult<K, N> doExecuteMultigetSlice(final Iterable<K> keys, final HSlicePredicate<N> workingSlicePredicate) {    
     return new ColumnFamilyResultWrapper<K, N>(keySerializer, topSerializer, 
         multigetSliceInternal(keys, workingSlicePredicate));
@@ -58,7 +59,30 @@ public class ThriftColumnFamilyTemplate<K, N> extends ColumnFamilyTemplate<K, N>
         multigetSliceInternal(keys, workingSlicePredicate), mapper);    
   }
 
-  
+  protected <V> ColumnFamilyResult<K, N> doExecuteIndexedSlices(final IndexedSlicesPredicate<K, N, V> predicate) {
+    return new ColumnFamilyResultWrapper<K, N>(keySerializer, topSerializer,
+        indexedSlicesInternal(predicate, activeSlicePredicate));
+  }
+
+  protected <V> ColumnFamilyResult<K, N> doExecuteIndexedSlices(IndexedSlicesPredicate<K, N, V> predicate,
+      HSlicePredicate<N> slicePredicate) {
+    return new ColumnFamilyResultWrapper<K, N>(keySerializer, topSerializer,
+        indexedSlicesInternal(predicate, slicePredicate));
+  }
+
+  protected <R,V> MappedColumnFamilyResult<K, N, R> doExecuteIndexedSlices(
+      IndexedSlicesPredicate<K, N, V> predicate,
+      ColumnFamilyRowMapper<K, N, R> mapper) {
+    return new MappedColumnFamilyResultWrapper<K,N,R>(keySerializer, topSerializer, 
+        indexedSlicesInternal(predicate, activeSlicePredicate), mapper);
+  }
+
+  protected <R, V> MappedColumnFamilyResult<K, N, R> doExecuteIndexedSlices(IndexedSlicesPredicate<K, N, V> predicate,
+      HSlicePredicate<N> slicePredicate, ColumnFamilyRowMapper<K, N, R> mapper) {
+    return new MappedColumnFamilyResultWrapper<K,N,R>(keySerializer, topSerializer, 
+        indexedSlicesInternal(predicate, slicePredicate), mapper);
+  }
+
   private ExecutionResult<Map<ByteBuffer, List<ColumnOrSuperColumn>>> sliceInternal(final K key,
       final HSlicePredicate<N> workingSlicePredicate) {
     return ((ExecutingKeyspace)keyspace).doExecuteOperation(new Operation<Map<ByteBuffer,List<ColumnOrSuperColumn>>>(OperationType.READ) {
@@ -80,7 +104,7 @@ public class ThriftColumnFamilyTemplate<K, N> extends ColumnFamilyTemplate<K, N>
       }
     });
   }
-  
+
   private ExecutionResult<Map<ByteBuffer, List<ColumnOrSuperColumn>>> multigetSliceInternal(final Iterable<K> keys,
       final HSlicePredicate<N> workingSlicePredicate) {
     return ((ExecutingKeyspace)keyspace).doExecuteOperation(new Operation<Map<ByteBuffer,List<ColumnOrSuperColumn>>>(OperationType.READ) {
@@ -94,7 +118,35 @@ public class ThriftColumnFamilyTemplate<K, N> extends ColumnFamilyTemplate<K, N>
             ThriftConverter.consistencyLevel(consistencyLevelPolicy.get(operationType)));
         } catch (Exception e) {
           throw exceptionsTranslator.translate(e);
-        }        
+        }
+      }
+    });
+  }
+
+  private <V> ExecutionResult<Map<ByteBuffer, List<ColumnOrSuperColumn>>> indexedSlicesInternal(
+      final IndexedSlicesPredicate<K, N, V> predicate, 
+      final HSlicePredicate<N> workingSlicePredicate) {
+    return ((ExecutingKeyspace)keyspace).doExecuteOperation(new Operation<Map<ByteBuffer,List<ColumnOrSuperColumn>>>(OperationType.READ) {
+      @Override
+      public Map<ByteBuffer, List<ColumnOrSuperColumn>> execute(Client cassandra) throws HectorException {
+        try {
+          List<KeySlice> keySlices = cassandra.get_indexed_slices(
+              columnParent,
+              predicate.toThrift(), 
+              workingSlicePredicate.toThrift(),
+              ThriftConverter.consistencyLevel(consistencyLevelPolicy.get(operationType)));
+          if (keySlices == null || keySlices.isEmpty()) {
+            return new LinkedHashMap<ByteBuffer, List<ColumnOrSuperColumn>>(0);
+          }
+          LinkedHashMap<ByteBuffer, List<ColumnOrSuperColumn>> ret = 
+              new LinkedHashMap<ByteBuffer, List<ColumnOrSuperColumn>>(keySlices.size());
+          for (KeySlice keySlice : keySlices) {
+            ret.put(ByteBuffer.wrap(keySlice.getKey()), keySlice.getColumns());
+          }
+          return ret;
+        } catch (Exception e) {
+          throw exceptionsTranslator.translate(e);
+        }
       }
     });
   }
