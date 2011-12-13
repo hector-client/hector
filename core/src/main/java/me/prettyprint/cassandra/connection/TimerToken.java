ab1b0ed8448f786b4e91a12f615c9fb6fd51ca26
zznate <nate@datastax.com>
2011-12-13 10:58:24 -0600

    Removes redundant tracking of Timer from Operation object, adds additional documentation

--- core/src/main/java/me/prettyprint/cassandra/connection/TimerToken.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/TimerToken.java
@@ -11,19 +11,33 @@ public interface TimerToken {
 
   /**
    * Implementations are free to return an underlying wrapper of some sort
-   * in order to retrieve additional context information
+   * in order to retrieve additional context information. See the
+   * {@link SpeedForJTimerToken} implementation for an example.
    * @return
    */
   Object getToken();
 
+  /**
+   * The {@link CassandraHost} against which we executed 
+   * @return
+   */
   CassandraHost getCassandraHost();
 
+  /**
+   *
+   * @return an arbitrary tag name. Implementation dependant.
+   */
   String getTagName();
 
+  /**
+   * enter the 'START' phase of execution. Implementations wishing to track timing
+   * would start the clock here
+   */
   void start();
 
   /**
-   * Stop this instance, filling in the details from the provided operation
+   * Stop this instance, filling in the details from the provided operation.
+   * Implementations implementing timing mechanisms would stop the clock here
    * @param op
    */
   void stop(Operation op);
