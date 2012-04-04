ae56f633b0f362fb93558ac70bef723ea1aefeb3
zznate <zznate.m@gmail.com>
2012-04-04 14:19:27 -0500

    added getPendingMutationSize to Mutator

--- core/src/test/java/me/prettyprint/cassandra/model/MutatorTest.java
+++ core/src/test/java/me/prettyprint/cassandra/model/MutatorTest.java
@@ -150,6 +150,7 @@ public class MutatorTest extends BaseEmbededServerSetupTest {
     for (int i = 0; i < 5; i++) {
       m.addInsertion("k" + i, cf, createColumn("name", "value" + i, se, se));
     }
+    assertEquals(5, m.getPendingMutationCount());
     MutationResult r = m.execute();
     assertTrue("Execute time should be > 0", r.getExecutionTimeMicro() > 0);
     assertTrue("Should have operated on a host", r.getHostUsed() != null);
