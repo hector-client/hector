094b3b7fdaae77e56b4c7ee50a0047d35afa6c75
zznate <zznate.m@gmail.com>
2012-07-11 09:47:11 -0500

    Revert "Merge pull request #492 from mebigfatguy/master"
    This reverts commit 407b24820207d078e3327d79b6f7d3381b2e5116, reversing
    changes made to 3011a08f5a2f4cdeaa3bd476624f72e101cc8b26.

--- core/src/main/java/me/prettyprint/cassandra/connection/LatencyAwareHClientPool.java
+++ core/src/main/java/me/prettyprint/cassandra/connection/LatencyAwareHClientPool.java
@@ -86,9 +86,4 @@ public class LatencyAwareHClientPool extends ConcurrentHClientPool {
   public boolean equals(Object obj) {
     return ((LatencyAwareHClientPool) obj).getCassandraHost().equals(getCassandraHost());
   }
-
-  @Override
-  public int hashCode() {
-    return getCassandraHost().hashCode();
-  }
 }
