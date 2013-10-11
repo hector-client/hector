42fac372ef97df3fc881b349a7c744b9c1f01f69
Shane Perry <shane@lingotek.com>
2013-10-11 11:00:24 -0600

    Call rebuild after configuring CFMetaData

--- test/src/main/java/me/prettyprint/hector/testutils/EmbeddedSchemaLoader.java
+++ test/src/main/java/me/prettyprint/hector/testutils/EmbeddedSchemaLoader.java
@@ -92,9 +92,9 @@ public class EmbeddedSchemaLoader {
         indexCFMD(ks1, "Indexed1", true),
         indexCFMD(ks1, "Indexed2", false),
         //new CFMetaData(ks1, "StandardInteger1", st, IntegerType.instance, null).keyCacheSize(0),
-        new CFMetaData(ks1, "Counter1", st, bytes, null).replicateOnWrite(true).defaultValidator(CounterColumnType.instance),
-        new CFMetaData(ks1, "Counter2", st, bytes, null).replicateOnWrite(true).defaultValidator(CounterColumnType.instance),
-        new CFMetaData(ks1, "SuperCounter1", su, bytes, bytes).replicateOnWrite(true).defaultValidator(CounterColumnType.instance),
+        new CFMetaData(ks1, "Counter1", st, bytes, null).replicateOnWrite(true).defaultValidator(CounterColumnType.instance).rebuild(),
+        new CFMetaData(ks1, "Counter2", st, bytes, null).replicateOnWrite(true).defaultValidator(CounterColumnType.instance).rebuild(),
+        new CFMetaData(ks1, "SuperCounter1", su, bytes, bytes).replicateOnWrite(true).defaultValidator(CounterColumnType.instance).rebuild(),
         jdbcCFMD(ks1, "JdbcInteger", IntegerType.instance),
         jdbcCFMD(ks1, "JdbcUtf8", UTF8Type.instance),
         jdbcCFMD(ks1, "JdbcLong", LongType.instance),
@@ -107,22 +107,22 @@ public class EmbeddedSchemaLoader {
   }
 
   private static CFMetaData compositeCFMD(String ksName, String cfName, AbstractType<?>... types) {
-      return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard, CompositeType.getInstance(Arrays.asList(types)), null);
+      return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard, CompositeType.getInstance(Arrays.asList(types)), null).rebuild();
   }
 
 	private static CFMetaData dynamicCompositeCFMD(String ksName, String cfName) {
-		return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard, DynamicCompositeType.getInstance(alias), null);
+		return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard, DynamicCompositeType.getInstance(alias), null).rebuild();
 	}
 
   private static CFMetaData standardCFMD(String ksName, String cfName) {
     return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard,
-        BytesType.instance, null);
+        BytesType.instance, null).rebuild();
   }
 
   private static CFMetaData superCFMD(String ksName, String cfName,
       AbstractType subcc) {
     return new CFMetaData(ksName, cfName, ColumnFamilyType.Super,
-        BytesType.instance, subcc);
+        BytesType.instance, subcc).rebuild();
   }
 
   private static CFMetaData indexCFMD(String ksName, String cfName, final Boolean withIdxType)
@@ -136,13 +136,13 @@ public class EmbeddedSchemaLoader {
                       //TODO: that last null is for composites. Need to understand that better, but null is reasonable
                       ColumnDefinition def = new org.apache.cassandra.config.ColumnDefinition(cName,LongType.instance,IndexType.KEYS,null,"birthyear_index",null, Type.REGULAR);
                       put (cName,def);
-                  }});
+                  }}).rebuild();
   }
 
   private static CFMetaData jdbcCFMD(String ksName, String cfName,
       AbstractType comp) {
     return new CFMetaData(ksName, cfName, ColumnFamilyType.Standard, comp, null)
-        .defaultValidator(comp);
+        .defaultValidator(comp).rebuild();
   }
 
   private static CFMetaData cqlTestCf(String ksName, String cfName,
@@ -154,7 +154,7 @@ public class EmbeddedSchemaLoader {
             {{
               ByteBuffer cName = ByteBuffer.wrap("birthyear".getBytes(Charsets.UTF_8));
               put(cName, new ColumnDefinition(cName, LongType.instance, null, null, null,null, Type.REGULAR));
-          }});
+          }}).rebuild();
   }
 }
 
